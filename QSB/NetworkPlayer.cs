1c302a931f00eba25d5ed3ce25322c1f4ab702d4
Ricardo Lopes <raicuparta@gmail.com>
2020-02-10 23:03:28 +0100

    Initial commit

---
+++ QSB/NetworkPlayer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+namespace QSB {
+    public class NetworkPlayer: NetworkBehaviour {
+        Transform _body;
+        Transform _sun;
+
+        void Start () {
+            QSB.Log("Started network player");
+
+            _sun = Locator.GetAstroObject(AstroObject.Name.TimberHearth).transform;
+
+            var player = Locator.GetPlayerBody().transform.Find("Traveller_HEA_Player_v2");
+            if (isLocalPlayer) {
+                _body = player;
+            } else {
+                _body = Instantiate(player);
+                _body.parent = transform;
+                _body.localPosition = Vector3.zero;
+                _body.localRotation = Quaternion.identity;
+            }
+        }
+        void Update () {
+            if (!_body) {
+                return;
+            }
+            if (isLocalPlayer) {
+                transform.position = _body.position - _sun.position;
+                transform.rotation = _body.rotation * Quaternion.Inverse(_sun.rotation);
+            } else {
+                _body.position = _sun.position + transform.position;
+                _body.rotation = transform.rotation * _sun.rotation;
+            }
+        }
+    }
+}
---
+++ QSB/QSB.cs
@@ -0,0 +1,56 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace QSB {
+    public class QSB: ModBehaviour {
+        static QSB _instance;
+
+        void Awake () {
+            Application.runInBackground = true;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+        void Update () {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+        void Start () {
+            _instance = this;
+
+            ModHelper.Events.Subscribe<PlayerBody>(Events.AfterStart);
+            ModHelper.Events.OnEvent += OnEvent;
+
+            var assetBundle = ModHelper.Assets.LoadBundle("assets/network");
+            var networkManager = Instantiate(assetBundle.LoadAsset<GameObject>("assets/networkmanager.prefab"));
+            var networkPlayerPrefab = assetBundle.LoadAsset<GameObject>("assets/networkplayer.prefab");
+            networkPlayerPrefab.AddComponent<NetworkPlayer>();
+            networkManager.GetComponent<NetworkManager>().playerPrefab = networkPlayerPrefab;
+        }
+
+        void OnEvent (MonoBehaviour behaviour, Events ev) {
+            var player = GameObject.Find("Traveller_HEA_Player_v2");
+
+            var spawn1 = Locator.GetPlayerBody().gameObject;
+            spawn1.AddComponent<NetworkStartPosition>();
+            var spawn2 = Locator.GetShipBody().gameObject;
+            spawn2.AddComponent<NetworkStartPosition>();
+
+            var networkIdentity = player.AddComponent<NetworkIdentity>();
+            networkIdentity.localPlayerAuthority = true;
+
+            var networkTransform = player.AddComponent<NetworkTransform>();
+
+            var networkManager = gameObject.AddComponent<NetworkManager>();
+            networkManager.playerPrefab = player;
+            var networkHUD = gameObject.AddComponent<NetworkManagerHUD>();
+        }
+
+        public static void Log (params string[] strings) {
+            _instance.ModHelper.Console.WriteLine(string.Join(" ", strings));
+        }
+    }
+}
