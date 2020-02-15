63ec5d80cf1ecd693ed11aac36f1c7b7b09e7084
Aleksander Waage <alek.ntnu@gmail.com>
2020-02-15 22:54:09 +0100

    nothing works

--- QSB/NetworkPlayer.cs
+++ QSB/NetworkPlayer.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.Networking;
 
 namespace QSB
@@ -36,6 +37,40 @@ namespace QSB
                 // todo disable movement
                 _body.Find("player_mesh_noSuit:Traveller_HEA_Player/player_mesh_noSuit:Player_Head").gameObject.layer = 0;
                 _body.Find("Traveller_Mesh_v01:Traveller_Geo/Traveller_Mesh_v01:PlayerSuit_Helmet").gameObject.layer = 0;
+            }
+
+            NetworkAnimator netAnim;
+            Animator anim;
+            try
+            {
+                netAnim = GetComponent<NetworkAnimator>();
+                netAnim.enabled = true;
+                anim = _body.GetComponent<Animator>();
+                netAnim.animator = anim;
+            }
+            catch (Exception ex)
+            {
+                DebugLog.Console("Error while doing normal anim stuff", ex);
+                return;
+            }
+
+            for (var i = 0; i < 20; i++)
+            {
+                try
+                {
+                    if (anim.GetParameter(i) != null)
+                    {
+                        netAnim.SetParameterAutoSend(i, true);
+                    }
+                    else
+                    {
+                        DebugLog.Console("Parameter", i, "is null");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    DebugLog.Console("Error while getting parameter", i, ex);
+                }
             }
 
         }
--- QSB/QSBNetworkManager.cs
+++ QSB/QSBNetworkManager.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Linq;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Networking;
 
 namespace QSB
@@ -12,42 +10,7 @@ namespace QSB
             var assetBundle = QSB.Helper.Assets.LoadBundle("assets/network");
             playerPrefab = assetBundle.LoadAsset<GameObject>("assets/networkplayer.prefab");
             playerPrefab.AddComponent<NetworkPlayer>();
-
-            try
-            {
-                var anim = playerPrefab.GetComponent<Animator>();
-                if (anim == null)
-                {
-                    DebugLog.Console("Error: Animator is null");
-                    anim = playerPrefab.GetComponentsInChildren<Animator>()
-                        .Single(x => x.name == "Traveller_HEA_Player_v2");
-                }
-
-                var netAnim = playerPrefab.AddComponent<NetworkAnimator>();
-                netAnim.animator = anim;
-                for (var i = 0; i < 20; i++)
-                {
-                    try
-                    {
-                        if (anim.GetParameter(i) != null)
-                        {
-                            netAnim.SetParameterAutoSend(i, true);
-                        }
-                        else
-                        {
-                            DebugLog.Console("Parameter", i, "is null");
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        DebugLog.Console("Error while getting parameter", i, ex);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                DebugLog.Console("Error setting up anim sync", ex);
-            }
+            playerPrefab.AddComponent<NetworkAnimator>().enabled = false;
         }
 
         public override void OnStartServer()
