4977d5e5378d78e78bd6d15387f2137d8b539d3b
Ricardo Lopes <raicuparta@gmail.com>
2020-02-18 20:41:58 +0100

    Create QSBBehaviour

---
+++ QSB/QSBBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace QSB {
+    abstract class QSBBehaviour: MonoBehaviour {
+        void Awake () {
+            GlobalMessenger.AddListener("WakeUp", PlayerWokeUp);
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        private void OnSceneLoaded (Scene scene, LoadSceneMode mode) {
+            if (scene.name == "SolarSystem") {
+                StartSolarSystem();
+            } else if (scene.name == "EyeOfTheUniverse") {
+                StartEyeOfUniverse();
+            }
+        }
+
+        protected virtual void PlayerWokeUp () { }
+        protected virtual void StartSolarSystem () { }
+        protected virtual void StartEyeOfUniverse () { }
+    }
+}
