ed67f8e7efefb9d04d9eb4afd7d24fbc76a87005
Aleksander Waage <alek.ntnu@gmail.com>
2020-02-19 05:27:24 +0100

    fixed smoothing velocity

---
+++ QSB/FloatParam.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace QSB
+{
+    public class FloatParam
+    {
+        public float Current { get; private set; }
+        public float Target { get; set; }
+
+        private float _velocity;
+
+        public float Smooth(float smoothTime)
+        {
+            Current = Mathf.SmoothDamp(Current, Target, ref _velocity, smoothTime);
+            return Current;
+        }
+
+    }
+}
