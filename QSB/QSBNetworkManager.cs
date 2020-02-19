1f10ae6983d97ed2ee43dcf935ac78115a4828ef
Ricardo Lopes <raicuparta@gmail.com>
2020-02-19 20:16:52 +0100

    Network ship being created from prefab

--- QSB/QSBNetworkManager.cs
+++ QSB/QSBNetworkManager.cs
@@ -5,12 +5,26 @@ namespace QSB
 {
     public class QSBNetworkManager : NetworkManager
     {
+        AssetBundle _assetBundle;
         private void Awake()
         {
-            var assetBundle = QSB.Helper.Assets.LoadBundle("assets/network");
-            playerPrefab = assetBundle.LoadAsset<GameObject>("assets/networkplayer.prefab");
+            _assetBundle = QSB.Helper.Assets.LoadBundle("assets/network");
+            playerPrefab = _assetBundle.LoadAsset<GameObject>("assets/networkplayer.prefab");
             playerPrefab.AddComponent<NetworkPlayer>();
             playerPrefab.AddComponent<AnimationSync>();
+
+            var prefab = _assetBundle.LoadAsset<GameObject>("assets/networkship.prefab");
+            //prefab.AddComponent<ShipTransformSync>();
+            spawnPrefabs.Add(prefab);
+        }
+
+        public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+        {
+            base.OnServerAddPlayer(conn, playerControllerId);
+
+            var prefab = _assetBundle.LoadAsset<GameObject>("assets/networkship.prefab");
+            prefab.AddComponent<ShipTransformSync>();
+            NetworkServer.SpawnWithClientAuthority(Instantiate(spawnPrefabs[0]), conn);
         }
 
         public override void OnStartServer()
@@ -26,6 +40,5 @@ namespace QSB
             gameObject.AddComponent<WakeUpSync>();
             gameObject.AddComponent<SectorSync>();
         }
-
     }
 }
---
+++ QSB/ShipTransformSync.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+namespace QSB
+{
+    class ShipTransformSync : TransformSync
+    {
+        Transform _shipModel;
+
+        Transform GetShipModel()
+        {
+            if (!_shipModel)
+            {
+                // TODO: specify ship model
+                _shipModel = Locator.GetShipBody().transform;
+            }
+            return _shipModel;
+        }
+
+        protected override Transform GetLocalTransform()
+        {
+            return GetShipModel();
+        }
+
+        protected override Transform GetRemoteTransform()
+        {
+            var shipModel = GetShipModel();
+            var cockpit = Instantiate(shipModel.Find("Module_Cockpit/Geo_Cockpit/Cockpit_Geometry/Cockpit_Exterior"));
+            var cabin = Instantiate(shipModel.Find("Module_Cabin/Geo_Cabin/Cabin_Geometry/Cabin_Exterior"));
+            var supplies = Instantiate(shipModel.Find("Module_Supplies/Geo_Supplies/Supplies_Geometry/Supplies_Exterior"));
+            var engine = Instantiate(shipModel.Find("Module_Engine/Geo_Engine/Engine_Geometry/Engine_Exterior"));
+            //var landingGear = shipModel.Find("Geo_LandingGear_Front");
+
+            var remoteTransform = new GameObject().transform;
+            remoteTransform.position = shipModel.position;
+            remoteTransform.rotation = shipModel.rotation;
+
+            cockpit.parent = cabin.parent = supplies.parent = engine.parent = remoteTransform;
+
+            return remoteTransform;
+        }
+    }
+}
