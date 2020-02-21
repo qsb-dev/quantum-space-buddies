7b9a7b0614476c3455e40247949ad100c24dab6c
Aleksander Waage <alek.ntnu@gmail.com>
2020-02-21 21:01:08 +0100

    message type thing

--- QSB.Messaging/MessageType.cs
+++ QSB.Messaging/MessageType.cs
@@ -4,7 +4,8 @@ namespace QSB.Messaging
 {
     public enum MessageType
     {
-        SectorSync = MsgType.Highest + 1,
-        WakeUpSync = MsgType.Highest + 2
+        Sector = MsgType.Highest + 1,
+        WakeUp = MsgType.Highest + 2
+        // Add other message types here, incrementing the value.
     }
 }
