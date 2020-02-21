e6b3bbde101eae975f92990dd2d8b65cf5f3f050
Aleksander Waage <alek.ntnu@gmail.com>
2020-02-21 21:14:23 +0100

    syncing animation triggers

--- QSB.Messaging/MessageType.cs
+++ QSB.Messaging/MessageType.cs
@@ -5,7 +5,8 @@ namespace QSB.Messaging
     public enum MessageType
     {
         Sector = MsgType.Highest + 1,
-        WakeUp = MsgType.Highest + 2
+        WakeUp = MsgType.Highest + 2,
+        AnimTrigger = MsgType.Highest + 3
         // Add other message types here, incrementing the value.
     }
 }
