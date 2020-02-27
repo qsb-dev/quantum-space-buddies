9ba3358c48189ce2308e0bb7eac93e4dabe6b852
Ricardo Lopes <raicuparta@gmail.com>
2020-02-27 19:56:40 +0100

    Refactor code

--- QSB/RespawnOnDeath.cs
+++ QSB/RespawnOnDeath.cs
@@ -29,6 +29,37 @@ namespace QSB
             _shipBody = Locator.GetShipBody();
         }
 
+        public void ResetShip()
+        {
+            _shipBody.SetVelocity(_shipSpawnPoint.GetPointVelocity());
+            _shipBody.WarpToPositionRotation(_shipSpawnPoint.transform.position, _shipSpawnPoint.transform.rotation);
+
+            // Reset ship damage.
+            if (Locator.GetShipTransform())
+            {
+                ShipComponent[] shipComponents = Locator.GetShipTransform().GetComponentsInChildren<ShipComponent>();
+                for (int i = 0; i < shipComponents.Length; i++)
+                {
+                    shipComponents[i].SetDamaged(false);
+                }
+            }
+        }
+
+        public void ResetPlayer()
+        {
+            typeof(PlayerState).SetValue("_insideShip", false);
+            var spawnPoint = GetSpawnPoint();
+            OWRigidbody playerBody = Locator.GetPlayerBody();
+            playerBody.WarpToPositionRotation(spawnPoint.transform.position, spawnPoint.transform.rotation);
+            playerBody.SetVelocity(spawnPoint.GetPointVelocity());
+            spawnPoint.AddObjectToTriggerVolumes(Locator.GetPlayerDetector().gameObject);
+            spawnPoint.AddObjectToTriggerVolumes(Locator.GetPlayerCamera().GetComponentInChildren<FluidDetector>().gameObject);
+            spawnPoint.OnSpawnPlayer();
+
+            // Reset fuel and health.
+            Locator.GetPlayerTransform().GetComponent<PlayerResources>().DebugRefillResources();
+        }
+
         SpawnPoint GetSpawnPoint(bool isShip = false)
         {
             var spawnList = _playerSpawner.GetValue<SpawnPoint[]>("_spawnList");
@@ -43,40 +74,12 @@ namespace QSB
             return null;
         }
 
-        public void ResetShip()
-        {
-            _shipBody.SetVelocity(_shipSpawnPoint.GetPointVelocity());
-            _shipBody.WarpToPositionRotation(_shipSpawnPoint.transform.position, _shipSpawnPoint.transform.rotation);
-        }
-
         internal static class Patches
         {
             public static bool PreFinishDeathSequence()
             {
-                // Teleport palyer to Timber Hearth.
-                typeof(PlayerState).SetValue("_insideShip", false);
-                var spawnPoint = Instance.GetSpawnPoint();
-                OWRigidbody playerBody = Locator.GetPlayerBody();
-                playerBody.WarpToPositionRotation(spawnPoint.transform.position, spawnPoint.transform.rotation);
-                playerBody.SetVelocity(spawnPoint.GetPointVelocity());
-                spawnPoint.AddObjectToTriggerVolumes(Locator.GetPlayerDetector().gameObject);
-                spawnPoint.AddObjectToTriggerVolumes(Locator.GetPlayerCamera().GetComponentInChildren<FluidDetector>().gameObject);
-                spawnPoint.OnSpawnPlayer();
-
-                // Reset fuel and health.
-                Locator.GetPlayerTransform().GetComponent<PlayerResources>().DebugRefillResources();
-
+                Instance.ResetPlayer();
                 Instance.ResetShip();
-
-                // Reset ship damage.
-                if (Locator.GetShipTransform())
-                {
-                    ShipComponent[] shipComponents = Locator.GetShipTransform().GetComponentsInChildren<ShipComponent>();
-                    for (int l = 0; l < shipComponents.Length; l++)
-                    {
-                        shipComponents[l].SetDamaged(false);
-                    }
-                }
 
                 // Prevent original death method from running.
                 return false;
