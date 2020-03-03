9ebc2746b8321cc28654293379fed8c5ca55f62d
Aleksander Waage <alek.ntnu@gmail.com>
2020-03-03 22:29:55 +0100

    saying hello

---
+++ QSB/Swag/HelloMessage.cs
@@ -0,0 +1,22 @@
+﻿using QSB.Messaging;
+using UnityEngine.Networking;
+
+namespace QSB.Swag
+{
+    public class HelloMessage : QSBMessage
+    {
+        public override MessageType MessageType => MessageType.Hello;
+
+        public string PlayerName { get; set; }
+
+        public override void Deserialize(NetworkReader reader)
+        {
+            PlayerName = reader.ReadString();
+        }
+
+        public override void Serialize(NetworkWriter writer)
+        {
+            writer.Write(PlayerName);
+        }
+    }
+}
