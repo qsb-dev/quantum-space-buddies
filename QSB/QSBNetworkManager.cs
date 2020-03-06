1249d20fe29209da794b11c678d6e2f823af9fb6
Aleksander Waage <alek.ntnu@gmail.com>
2020-03-06 18:38:25 +0100

    emptied the update method of NetworkManagerHUD

--- QSB/QSBNetworkManager.cs
+++ QSB/QSBNetworkManager.cs
@@ -68,6 +68,8 @@ namespace QSB
             connectionConfig.AddChannel(QosType.Unreliable);
             channels.Add(QosType.Reliable);
             channels.Add(QosType.Unreliable);
+
+            QSB.Helper.HarmonyHelper.EmptyMethod<NetworkManagerHUD>("Update");
         }
 
         public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
