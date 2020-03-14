e690958f0f75a57537229900fb4ef0a0aeb6623c
Ricardo Lopes <Raicuparta@users.noreply.github.com>
2020-03-14 15:42:51 +0100

    Fast forward speed proportional to client / server time diff (#74)
    * Remove wake up dependency from transform sync
    
    * Remove wake up dependency for all synced transforms
    
    * Remove comments
    
    * Remove obsolete QSBBehaviour component
    
    * Remove wakeup dependency in RespawnOnDeath
    
    * Prevent errors on MessageHandler and RespawnOnDeath when network not initialized
    
    * Add RespawnOnDeath only after network is initialized
    
    * wip
    
    * Prevent trying to add animator components multiple times
    
    * Reset sectors on scene load
    
    * Reset transform sync on scene change
    
    * Remove comment
    
    * Remove wakeup event dependency from WakeUpSync
    
    * Sync loop count
    
    * Fast forward speed dependent on difference between client and server time
    
    * Lerp on update
    
    * Change fastforward condition
    
    * Update WakeUpSync.cs

--- QSB/TimeSync/PreserveTimeScale.cs
+++ QSB/TimeSync/PreserveTimeScale.cs
@@ -15,18 +15,11 @@ namespace QSB.TimeSync
                 return;
             }
 
-            GlobalMessenger.AddListener("GamePaused", OnPause);
-
             var campfires = GameObject.FindObjectsOfType<Campfire>();
             foreach (var campfire in campfires)
             {
                 campfire.SetValue("_canSleepHere", false);
             }
-        }
-
-        private void OnPause()
-        {
-            Time.timeScale = 1;
         }
 
     }
