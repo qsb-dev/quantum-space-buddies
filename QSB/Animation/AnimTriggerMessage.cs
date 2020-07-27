46deed3d4e8a2ae392e4ddae48bacb8271add8ef
AmazingAlek <alek.ntnu@gmail.com>
2020-07-28 01:14:32 +0200

    sync crouching (#97)

--- QSB/Animation/AnimTriggerMessage.cs
+++ QSB/Animation/AnimTriggerMessage.cs
@@ -9,15 +9,18 @@ namespace QSB.Animation
 
         public short TriggerId;
         public uint SenderId;
+        public float Value;
 
         public override void Deserialize(NetworkReader reader)
         {
+            Value = reader.ReadSingle();
             TriggerId = reader.ReadInt16();
             SenderId = reader.ReadPackedUInt32();
         }
 
         public override void Serialize(NetworkWriter writer)
         {
+            writer.Write(Value);
             writer.Write(TriggerId);
             writer.Write(SenderId);
         }
