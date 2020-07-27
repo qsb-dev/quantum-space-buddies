1f70967622d8e8fe0beeab3781646bb941a4861b
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2020-07-27 14:13:07 +0100

    format

--- QSB/Animation/QSBFlashlight.cs
+++ QSB/Animation/QSBFlashlight.cs
@@ -23,42 +23,42 @@ namespace QSB.Animation
             _baseRotation = _basePivot.rotation;
         }
 
-		public void TurnOn()
-		{
-			if (!_flashlightOn)
-			{
-				for (int i = 0; i < _lights.Length; i++)
-				{
-					_lights[i].GetLight().enabled = true;
-				}
-				_flashlightOn = true;
-				Quaternion rotation = _root.rotation;
-				_basePivot.rotation = rotation;
-				_baseRotation = rotation;
-				_baseForward = _basePivot.forward;
-			}
-		}
+        public void TurnOn()
+        {
+            if (!_flashlightOn)
+            {
+                for (int i = 0; i < _lights.Length; i++)
+                {
+                    _lights[i].GetLight().enabled = true;
+                }
+                _flashlightOn = true;
+                Quaternion rotation = _root.rotation;
+                _basePivot.rotation = rotation;
+                _baseRotation = rotation;
+                _baseForward = _basePivot.forward;
+            }
+        }
 
-		public void TurnOff()
-		{
-			if (_flashlightOn)
-			{
-				for (int i = 0; i < _lights.Length; i++)
-				{
-					_lights[i].GetLight().enabled = false;
-				}
-				_flashlightOn = false;
-			}
-		}
+        public void TurnOff()
+        {
+            if (_flashlightOn)
+            {
+                for (int i = 0; i < _lights.Length; i++)
+                {
+                    _lights[i].GetLight().enabled = false;
+                }
+                _flashlightOn = false;
+            }
+        }
 
-		private void FixedUpdate()
-		{
-			Quaternion lhs = Quaternion.FromToRotation(_basePivot.up, _root.up) * Quaternion.FromToRotation(_baseForward, _root.forward);
-			Quaternion b = lhs * _baseRotation;
-			_baseRotation = Quaternion.Slerp(_baseRotation, b, 6f * Time.deltaTime);
-			_basePivot.rotation = _baseRotation;
-			_baseForward = _basePivot.forward;
-			_wobblePivot.localRotation = OWUtilities.GetWobbleRotation(0.3f, 0.15f) * Quaternion.identity;
-		}
-	}
+        private void FixedUpdate()
+        {
+            Quaternion lhs = Quaternion.FromToRotation(_basePivot.up, _root.up) * Quaternion.FromToRotation(_baseForward, _root.forward);
+            Quaternion b = lhs * _baseRotation;
+            _baseRotation = Quaternion.Slerp(_baseRotation, b, 6f * Time.deltaTime);
+            _basePivot.rotation = _baseRotation;
+            _baseForward = _basePivot.forward;
+            _wobblePivot.localRotation = OWUtilities.GetWobbleRotation(0.3f, 0.15f) * Quaternion.identity;
+        }
+    }
 }
