3e8e5dd49643995edc78ab64e5892d5c507db797
Aleksander Waage <alek.ntnu@gmail.com>
2020-08-04 17:36:23 +0200

    .

--- QSB/QSBNetworkManager.cs
+++ QSB/QSBNetworkManager.cs
@@ -57,7 +57,7 @@ namespace QSB
             playerPrefab.AddComponent<PlayerTransformSync>();
             playerPrefab.AddComponent<AnimationSync>();
             playerPrefab.AddComponent<WakeUpSync>();
-            
+
             _shipPrefab = _assetBundle.LoadAsset<GameObject>("assets/networkship.prefab");
             _shipPrefab.AddComponent<ShipTransformSync>();
             spawnPrefabs.Add(_shipPrefab);
