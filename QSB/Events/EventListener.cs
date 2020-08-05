3c6e4af85a3ad81c7e541386139376a4040b46f5
Aleksander Waage <alek.ntnu@gmail.com>
2020-08-05 21:13:25 +0200

    i dunno how to test this but i think this is the same

--- QSB/Events/EventListener.cs
+++ QSB/Events/EventListener.cs
@@ -8,44 +8,36 @@ namespace QSB.Events
 {
     public class EventListener : MonoBehaviour
     {
-        public Dictionary<EventType, List<Type>> TypedList = new Dictionary<EventType, List<Type>>
+        private readonly Dictionary<EventType, List<Type>> _typedList = new Dictionary<EventType, List<Type>>
         {
-            {EventType.EquipSignalscope, new List<Type>{typeof(Signalscope)}},
-            {EventType.ProbeLauncherEquipped, new List<Type>{typeof(ProbeLauncher)}},
-            {EventType.ProbeLauncherUnequipped, new List<Type>{typeof(ProbeLauncher)}},
-            {EventType.RetrieveProbe, new List<Type>{typeof(SurveyorProbe)}},
-            {EventType.LaunchProbe, new List<Type>{typeof(SurveyorProbe)}}
+            { EventType.EquipSignalscope, new List<Type> { typeof(Signalscope)} },
+            { EventType.ProbeLauncherEquipped, new List<Type> { typeof(ProbeLauncher) } },
+            { EventType.ProbeLauncherUnequipped, new List<Type> { typeof(ProbeLauncher) } },
+            { EventType.RetrieveProbe, new List<Type> { typeof(SurveyorProbe) } },
+            { EventType.LaunchProbe, new List<Type> { typeof(SurveyorProbe) } }
         };
 
         private void Awake()
         {
             foreach (var item in Enum.GetNames(typeof(EventType)))
             {
-                if (!TypedList.Keys.Contains((EventType)Enum.Parse(typeof(EventType), item)))
+                if (!_typedList.Keys.Contains((EventType)Enum.Parse(typeof(EventType), item)))
                 {
                     GlobalMessenger.AddListener(item, () => SendEvent(item));
                 }
             }
-            // the following code is garbage and stupid and i should be ashamed
-            // it's too hot right now i just cant be bothered
-            MethodInfo oldMethod;
-            MethodInfo newMethod;
-            foreach (var item in TypedList)
+
+            foreach (var item in _typedList)
             {
-                switch (item.Value.Count)
-                {
-                    case 1:
-                        oldMethod = GetGenericMethod(typeof(EventListener), "Listen", 1);
-                        newMethod = oldMethod.MakeGenericMethod(item.Value[0]);
-                        newMethod.Invoke(this, new[] { (object)Enum.GetName(typeof(EventType), item.Key) }); // I HATE THIS
-                        break;
-                    case 2:
-                        oldMethod = GetGenericMethod(typeof(EventListener), "Listen", 2);
-                        newMethod = oldMethod.MakeGenericMethod(item.Value[0], item.Value[1]);
-                        newMethod.Invoke(this, new[] { (object)Enum.GetName(typeof(EventType), item.Key) });
-                        break;
-                }
+                InvokeGenericMethod(item.Key, item.Value);
             }
+        }
+
+        private void InvokeGenericMethod(EventType eventType, List<Type> items)
+        {
+            var oldMethod = GetGenericMethod(typeof(EventListener), "Listen", items.Count);
+            var newMethod = oldMethod.MakeGenericMethod(items.ToArray());
+            newMethod.Invoke(this, new[] { (object)Enum.GetName(typeof(EventType), eventType) });
         }
 
         private void SendEvent(string eventName)
@@ -55,30 +47,25 @@ namespace QSB.Events
 
         public void Listen<T>(string eventName)
         {
-            GlobalMessenger<T>.AddListener(eventName, (T var) => SendEvent(eventName));
+            GlobalMessenger<T>.AddListener(eventName, var => SendEvent(eventName));
         }
 
-        public void Listen<T,U>(string eventName)
+        public void Listen<T, TU>(string eventName)
         {
-            GlobalMessenger<T,U>.AddListener(eventName, (T var, U var2) => SendEvent(eventName));
+            GlobalMessenger<T, TU>.AddListener(eventName, (var, var2) => SendEvent(eventName));
         }
 
-        private MethodInfo GetGenericMethod(Type type, string name, int typeCount)
+        private MethodInfo GetGenericMethod(Type type, string methodName, int typeCount)
         {
-            var methods = from m in type.GetMethods()
-                          where m.Name == name
-                             && m.IsGenericMethodDefinition
-
-                          let typeParams = m.GetGenericArguments()
-                          let normalParams = m.GetParameters()
-
-                          where typeParams.Length == typeCount
-
-                          select m;
+            var methods = type.GetMethods()
+                .Where(m => m.Name == methodName && m.IsGenericMethodDefinition)
+                .Select(m => new { m, typeParams = m.GetGenericArguments() })
+                .Select(t => new { t, normalParams = t.m.GetParameters() })
+                .Where(t => t.t.typeParams.Length == typeCount)
+                .Select(t => t.t.m);
 
             return methods.Single();
         }
 
-        // this is the file i hate most in QSB. it should burn forever.
     }
 }
