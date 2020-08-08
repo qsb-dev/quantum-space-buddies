e4116244adcf15afd725562fa4c278b70393e6bb
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2020-08-08 13:47:33 +0100

    change

--- QSB/TransformSync/PlayerHUDMarker.cs
+++ QSB/TransformSync/PlayerHUDMarker.cs
@@ -14,7 +14,7 @@ namespace QSB.TransformSync
             _markerTarget = new GameObject().transform;
             _markerTarget.parent = transform;
 
-            _markerTarget.localPosition = new Vector3(0, 2, 0);
+            _markerTarget.localPosition = Vector3.up * 2;
         }
 
         public void Init(PlayerInfo player)
