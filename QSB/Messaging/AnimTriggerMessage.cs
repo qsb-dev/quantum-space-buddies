f7224c238248e1de82cf666117b0979a434eed1b
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2020-08-10 17:11:14 +0100

    fix crouching????

--- QSB/Messaging/AnimTriggerMessage.cs
+++ QSB/Messaging/AnimTriggerMessage.cs
@@ -3,21 +3,24 @@ using UnityEngine.Networking;
 
 namespace QSB.Messaging
 {
-    public class AnimTriggerMessage : PlayerMessage
+    public class AnimTriggerMessage : MessageBase
     {
         public short TriggerId;
+        public uint SenderId;
         public float Value;
 
         public override void Deserialize(NetworkReader reader)
         {
             Value = reader.ReadSingle();
             TriggerId = reader.ReadInt16();
+            SenderId = reader.ReadPackedUInt32();
         }
 
         public override void Serialize(NetworkWriter writer)
         {
             writer.Write(Value);
             writer.Write(TriggerId);
+            writer.Write(SenderId);
         }
     }
 }
