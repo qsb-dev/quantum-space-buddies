4ab353070d29307cb26241f3d16e61c42be59c65
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2020-08-23 12:43:08 +0100

    fix hud marker

--- QSB/Events/PlayerLeaveEvent.cs
+++ QSB/Events/PlayerLeaveEvent.cs
@@ -31,6 +31,7 @@ namespace QSB.Events
         {
             var playerName = PlayerRegistry.GetPlayer(message.AboutId).Name;
             DebugLog.ToAll($"{playerName} disconnected.", MessageType.Info);
+            PlayerRegistry.GetPlayer(message.AboutId).HudMarker?.Remove();
             PlayerRegistry.RemovePlayer(message.AboutId);
             message.NetIds.ToList().ForEach(netId => QSBNetworkManager.Instance.CleanupNetworkBehaviour(netId));
         }
