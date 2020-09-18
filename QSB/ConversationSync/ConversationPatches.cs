3fe4faa0fa7a37d8d948fa04cb3db2ff65806ddb
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2020-09-18 15:32:22 +0100

    add stuff

---
+++ QSB/ConversationSync/ConversationPatches.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QSB.ConversationSync
+{
+    public static class ConversationPatches
+    {
+        public static class OrbPatches
+        {
+            public static void ZoneExit(CharacterAnimController __instance)
+            {
+
+            }
+
+            public static void AddPatches()
+            {
+                QSB.Helper.HarmonyHelper.AddPostfix<NomaiInterfaceOrb>("StartDragFromPosition", typeof(OrbPatches), nameof(StartDragCallEvent));
+                QSB.Helper.HarmonyHelper.AddPrefix<NomaiInterfaceSlot>("CheckOrbCollision", typeof(OrbPatches), nameof(CheckOrbCollision));
+            }
+        }
+    }
+}
---
+++ QSB/ConversationSync/QSBCharacterAnimController.cs
@@ -0,0 +1,13 @@
+﻿using QSB.WorldSync;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QSB.ConversationSync
+{
+    public class QSBCharacterAnimController : WorldObject
+    {
+        public CharacterAnimController AnimController { get; private set; }
+    }
+}
