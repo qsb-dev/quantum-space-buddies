d7de691807ae295bcfa760da0364352bd1393894
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2020-12-19 19:06:13 +0000

    make timesyncui count down

--- QSB/TimeSync/TimeSyncUI.cs
+++ QSB/TimeSync/TimeSyncUI.cs
@@ -9,6 +9,8 @@ namespace QSB.TimeSync
 	{
 		public static TimeSyncUI Instance;
 
+		public static float TargetTime;
+
 		private Canvas _canvas;
 		private Text _text;
 		private float _startTime;
@@ -70,7 +72,7 @@ namespace QSB.TimeSync
 			{
 				return;
 			}
-			var totalSeconds = Mathf.Max(Time.timeSinceLevelLoad - _startTime, 0f);
+			var totalSeconds = Mathf.Max(TargetTime - Time.timeSinceLevelLoad, 0f);
 			var minutes = Mathf.FloorToInt(totalSeconds / 60f);
 			var seconds = Mathf.FloorToInt(totalSeconds) % 60;
 			var text = "";
