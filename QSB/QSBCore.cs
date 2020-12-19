99bac5b22b8d6ab97ca583c33cecac0a77546961
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2020-12-19 16:17:50 +0000

    change license

--- QSB/QSBCore.cs
+++ QSB/QSBCore.cs
@@ -14,20 +14,9 @@ using QuantumUNET.Components;
 using UnityEngine;
 
 /*
-Copyright (C) 2020 Henry Pointer (_nebula / misternebula), Aleksander Waage (AmazingAlek), Ricardo Lopes (Raicuparta)
-
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU Affero General Public License as published
-	by the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-	GNU Affero General Public License for more details.
-
-	You should have received a copy of the GNU Affero General Public License
-	along with this program. If not, see https://www.gnu.org/licenses/.
+	Copyright (C) 2020 Henry Pointer (_nebula / misternebula), Aleksander Waage (AmazingAlek), Ricardo Lopes (Raicuparta)
+	
+	Consult LICENSE file for full license.
 */
 
 namespace QSB
