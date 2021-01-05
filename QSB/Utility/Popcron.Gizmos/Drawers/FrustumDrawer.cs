529a8fa3f4b0b10101f13d7f218ad1da8a8e2145
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2021-01-05 16:38:13 +0000

    Update FrustumDrawer.cs

--- QSB/Utility/Popcron.Gizmos/Drawers/FrustumDrawer.cs
+++ QSB/Utility/Popcron.Gizmos/Drawers/FrustumDrawer.cs
@@ -14,11 +14,6 @@ namespace Popcron
 		{
 			var camera = (OWCamera)values[0];
 
-			if (camera == null)
-			{
-				DebugLog.DebugWrite("camera null");
-			}
-
 			//bottom left
 			buffer[0] = camera.ScreenToWorldPoint(new Vector3(0, 0, camera.nearClipPlane));
 			buffer[1] = camera.ScreenToWorldPoint(new Vector3(0, 0, camera.farClipPlane));
@@ -32,17 +27,17 @@ namespace Popcron
 			buffer[6] = camera.ScreenToWorldPoint(new Vector3(camera.pixelWidth, camera.pixelHeight, camera.nearClipPlane));
 			buffer[7] = camera.ScreenToWorldPoint(new Vector3(camera.pixelWidth, camera.pixelHeight, camera.farClipPlane));
 			//bottom left to bottom right
-			buffer[8] = camera.ScreenToWorldPoint(new Vector3(0, 0, camera.farClipPlane));
-			buffer[9] = camera.ScreenToWorldPoint(new Vector3(camera.pixelWidth, 0, camera.farClipPlane));
+			buffer[8] = camera.ScreenToWorldPoint(new Vector3(0, 0, camera.farClipPlane / 2));
+			buffer[9] = camera.ScreenToWorldPoint(new Vector3(camera.pixelWidth, 0, camera.farClipPlane / 2));
 			//bottom right to top right
-			buffer[10] = camera.ScreenToWorldPoint(new Vector3(camera.pixelWidth, 0, camera.farClipPlane));
-			buffer[11] = camera.ScreenToWorldPoint(new Vector3(camera.pixelWidth, camera.pixelHeight, camera.farClipPlane));
+			buffer[10] = camera.ScreenToWorldPoint(new Vector3(camera.pixelWidth, 0, camera.farClipPlane / 2));
+			buffer[11] = camera.ScreenToWorldPoint(new Vector3(camera.pixelWidth, camera.pixelHeight, camera.farClipPlane / 2));
 			//top right to top left
-			buffer[12] = camera.ScreenToWorldPoint(new Vector3(camera.pixelWidth, camera.pixelHeight, camera.farClipPlane));
-			buffer[13] = camera.ScreenToWorldPoint(new Vector3(0, camera.pixelHeight, camera.farClipPlane));
+			buffer[12] = camera.ScreenToWorldPoint(new Vector3(camera.pixelWidth, camera.pixelHeight, camera.farClipPlane / 2));
+			buffer[13] = camera.ScreenToWorldPoint(new Vector3(0, camera.pixelHeight, camera.farClipPlane / 2));
 			//top left to bottom left
-			buffer[14] = camera.ScreenToWorldPoint(new Vector3(0, camera.pixelHeight, camera.farClipPlane));
-			buffer[15] = camera.ScreenToWorldPoint(new Vector3(0, 0, camera.farClipPlane));
+			buffer[14] = camera.ScreenToWorldPoint(new Vector3(0, camera.pixelHeight, camera.farClipPlane / 2));
+			buffer[15] = camera.ScreenToWorldPoint(new Vector3(0, 0, camera.farClipPlane / 2));
 			return 16;
 		}
 	}
