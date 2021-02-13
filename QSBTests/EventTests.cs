32f931c44029b29c56b275756bdaf84359d0e3c3
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2021-02-13 09:07:39 +0000

    cleanup

--- QSBTests/EventTests.cs
+++ QSBTests/EventTests.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QSB.Events;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using QSB.Events;
 
 namespace QSBTests
 {
