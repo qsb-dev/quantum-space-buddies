1c4f17550a1df86a3f27c8ba12ceb9ec1452cd35
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2021-02-15 10:29:24 +0000

    add patch to set surface state to -1 on start

--- QSB/QuantumSync/Patches/ClientQuantumPatches.cs
+++ QSB/QuantumSync/Patches/ClientQuantumPatches.cs
@@ -1,4 +1,5 @@
 ﻿using QSB.Patches;
+using System.Reflection;
 
 namespace QSB.QuantumSync.Patches
 {
@@ -6,8 +7,20 @@ namespace QSB.QuantumSync.Patches
 	{
 		public override QSBPatchTypes Type => QSBPatchTypes.OnNonServerClientConnect;
 
-		public override void DoPatches() => QSBCore.Helper.HarmonyHelper.AddPrefix<QuantumMoon>("ChangeQuantumState", typeof(ClientQuantumPatches), nameof(ReturnFalsePatch));
-		public override void DoUnpatches() => QSBCore.Helper.HarmonyHelper.Unpatch<QuantumMoon>("ChangeQuantumState");
+		public override void DoPatches()
+		{
+			QSBCore.Helper.HarmonyHelper.AddPrefix<QuantumMoon>("ChangeQuantumState", typeof(ClientQuantumPatches), nameof(ReturnFalsePatch));
+			QSBCore.Helper.HarmonyHelper.AddPostfix<QuantumMoon>("Start", typeof(ClientQuantumPatches), nameof(Moon_CollapseOnStart));
+		}
+
+		public override void DoUnpatches()
+		{
+			QSBCore.Helper.HarmonyHelper.Unpatch<QuantumMoon>("ChangeQuantumState");
+			QSBCore.Helper.HarmonyHelper.Unpatch<QuantumMoon>("Start");
+		}
+
+		public static void Moon_CollapseOnStart(QuantumMoon __instance) 
+			=> __instance.GetType().GetMethod("SetSurfaceState", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(__instance, new object[] { -1 });
 
 		public static bool ReturnFalsePatch() => false;
 	}
