329bd3de5a2b6fb046d6bb7e4f04e60367ff0b46
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2021-02-16 12:45:46 +0000

    Update UnitTestDetector.cs

--- QSB/Utility/UnitTestDetector.cs
+++ QSB/Utility/UnitTestDetector.cs
@@ -5,13 +5,13 @@ namespace QSB.Utility
 {
 	public static class UnitTestDetector
 	{
+		public static bool IsInUnitTest { get; private set; }
+
 		static UnitTestDetector()
 		{
 			var testAssemblyName = "Microsoft.VisualStudio.TestPlatform.TestFramework";
 			IsInUnitTest = AppDomain.CurrentDomain.GetAssemblies()
 				.Any(a => a.FullName.StartsWith(testAssemblyName));
 		}
-
-		public static bool IsInUnitTest { get; private set; }
 	}
 }
