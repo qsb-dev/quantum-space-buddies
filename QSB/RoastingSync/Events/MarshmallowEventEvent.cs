b3d09cbb3e57bd98ebed3bc53f8a35cc99a5f279
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2021-03-31 10:59:02 +0100

    tossing marshmallows

--- QSB/RoastingSync/Events/MarshmallowEventEvent.cs
+++ QSB/RoastingSync/Events/MarshmallowEventEvent.cs
@@ -1,13 +1,16 @@
-﻿using QSB.Events;
+﻿using OWML.Utils;
+using QSB.Events;
 using QSB.Messaging;
 using QSB.Player;
 using QSB.Utility;
+using System.Linq;
+using UnityEngine;
 
 namespace QSB.RoastingSync.Events
 {
 	internal class MarshmallowEventEvent : QSBEvent<EnumMessage<MarshmallowEventType>>
 	{
-		public override EventType Type => EventType.MarshmallowEvent;
+		public override QSB.Events.EventType Type => QSB.Events.EventType.MarshmallowEvent;
 
 		public override void SetupListener() => GlobalMessenger<MarshmallowEventType>.AddListener(EventNames.QSBMarshmallowEvent, Handler);
 		public override void CloseListener() => GlobalMessenger<MarshmallowEventType>.RemoveListener(EventNames.QSBMarshmallowEvent, Handler);
@@ -22,6 +25,10 @@ namespace QSB.RoastingSync.Events
 
 		public override void OnReceiveRemote(bool server, EnumMessage<MarshmallowEventType> message)
 		{
+			if (!QSBCore.HasWokenUp)
+			{
+				return;
+			}
 			var marshmallow = QSBPlayerManager.GetPlayer(message.AboutId).Marshmallow;
 			if (marshmallow == null)
 			{
@@ -46,9 +53,34 @@ namespace QSB.RoastingSync.Events
 					marshmallow.Shrivel();
 					break;
 				case MarshmallowEventType.Toss:
-					//TODO : implement
+					TossMarshmallow(message.AboutId);
 					break;
 			}
+		}
+
+		private void TossMarshmallow(uint playerId)
+		{
+			var player = QSBPlayerManager.GetPlayer(playerId);
+			var stick = player.RoastingStick;
+			var stickTip = stick.transform.GetChild(0);
+
+			var mallowPrefab = Resources.FindObjectsOfTypeAll<RoastingStickController>().First().GetValue<GameObject>("_mallowBodyPrefab");
+
+			var tossedMallow = Object.Instantiate(mallowPrefab, stickTip.position, stickTip.rotation);
+			var rigidbody = tossedMallow.GetComponent<OWRigidbody>();
+			if (player.Campfire == null)
+			{
+				DebugLog.DebugWrite($"Error - Campfire for {playerId} is null.", OWML.Common.MessageType.Error);
+				return;
+			}
+			rigidbody.SetVelocity(player.Campfire.AttachedObject.GetAttachedOWRigidbody(false).GetPointVelocity(stickTip.position) + (stickTip.forward * 3f));
+			rigidbody.SetAngularVelocity(stickTip.right * 10f);
+			if (player.Marshmallow == null)
+			{
+				DebugLog.DebugWrite($"Error - Marshmallow for {playerId} is null.", OWML.Common.MessageType.Error);
+				return;
+			}
+			tossedMallow.GetComponentInChildren<MeshRenderer>().material.color = player.Marshmallow._burntColor;
 		}
 	}
 }
