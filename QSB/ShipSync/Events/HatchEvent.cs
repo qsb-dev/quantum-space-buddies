a250a222a0e3558586058fa581c05bcbc00820a6
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2021-04-12 11:43:22 +0100

    change hatch patch to be open and close

---
+++ QSB/ShipSync/Events/HatchEvent.cs
@@ -0,0 +1,39 @@
+﻿using OWML.Utils;
+using QSB.Events;
+using QSB.Messaging;
+
+namespace QSB.ShipSync.Events
+{
+	class HatchEvent : QSBEvent<BoolMessage>
+	{
+		public override EventType Type => EventType.OpenHatch;
+
+		public override void SetupListener() 
+			=> GlobalMessenger<bool>.AddListener(EventNames.QSBHatchState, Handler);
+
+		public override void CloseListener() 
+			=> GlobalMessenger<bool>.RemoveListener(EventNames.QSBHatchState, Handler);
+
+		private void Handler(bool open) => SendEvent(CreateMessage(open));
+
+		private BoolMessage CreateMessage(bool open) => new BoolMessage
+		{
+			AboutId = LocalPlayerId,
+			Value = open
+		};
+
+		public override void OnReceiveRemote(bool server, BoolMessage message)
+		{
+			var shipTransform = Locator.GetShipTransform();
+			var hatchController = shipTransform.GetComponentInChildren<HatchController>();
+			if (message.Value)
+			{
+				hatchController.Invoke("OpenHatch");
+			}
+			else
+			{
+				hatchController.Invoke("CloseHatch");
+			}
+		}
+	}
+}
--- QSB/ShipSync/Patches/ShipPatches.cs
+++ QSB/ShipSync/Patches/ShipPatches.cs
@@ -7,15 +7,30 @@ namespace QSB.ShipSync.Patches
 	{
 		public override QSBPatchTypes Type => QSBPatchTypes.OnClientConnect;
 
-		public override void DoPatches() 
-			=> QSBCore.HarmonyHelper.AddPrefix<HatchController>("OnPressInteract", typeof(ShipPatches), nameof(HatchController_OnPressInteract));
+		public override void DoPatches()
+		{
+			QSBCore.HarmonyHelper.AddPrefix<HatchController>("OnPressInteract", typeof(ShipPatches), nameof(HatchController_OnPressInteract));
+			QSBCore.HarmonyHelper.AddPrefix<HatchController>("OnEntry", typeof(ShipPatches), nameof(HatchController_OnEntry));
+		}
 
-		public override void DoUnpatches() 
-			=> QSBCore.HarmonyHelper.Unpatch<HatchController>("OnPressInteract");
+		public override void DoUnpatches()
+		{
+			QSBCore.HarmonyHelper.Unpatch<HatchController>("OnPressInteract");
+			QSBCore.HarmonyHelper.Unpatch<HatchController>("OnEntry");
+		}
 
 		public static bool HatchController_OnPressInteract()
 		{
-			QSBEventManager.FireEvent(EventNames.QSBOpenHatch);
+			QSBEventManager.FireEvent(EventNames.QSBHatchState, true);
+			return true;
+		}
+
+		public static bool HatchController_OnEntry(GameObjectActivationTrigger hitObj)
+		{
+			if (hitObj.CompareTag("PlayerDetector"))
+			{
+				QSBEventManager.FireEvent(EventNames.QSBHatchState, false);
+			}
 			return true;
 		}
 	}
