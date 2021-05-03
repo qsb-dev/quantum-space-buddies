0be93067784e0dcc55939aa97849a8461fb56e0e
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2021-05-03 23:14:38 +0100

    cleanup

--- QSB/Animation/Player/Thrusters/FlamesAndBubbles/SetUpThrusterFlames.cs
+++ QSB/Animation/Player/Thrusters/FlamesAndBubbles/SetUpThrusterFlames.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace QSB.Animation.Player.Thrusters.FlamesAndBubbles
+﻿namespace QSB.Animation.Player.Thrusters.FlamesAndBubbles
 {
-	class SetUpThrusterFlames
+	internal class SetUpThrusterFlames
 	{
 	}
 }
