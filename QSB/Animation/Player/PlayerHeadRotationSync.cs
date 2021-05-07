64280dcc4c4511a3e629b572d6492ee618c1e13f
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2021-05-07 22:02:50 +0100

    undo default-config change and add PlayerHeadRotationSync log

--- QSB/Animation/Player/PlayerHeadRotationSync.cs
+++ QSB/Animation/Player/PlayerHeadRotationSync.cs
@@ -12,6 +12,7 @@ namespace QSB.Animation.Player
 
 		public void Init(Transform lookBase)
 		{
+			DebugLog.DebugWrite($"Init - attached to {gameObject.name}");
 			_attachedAnimator = GetComponent<Animator>();
 			_lookBase = lookBase;
 			_isSetUp = true;
