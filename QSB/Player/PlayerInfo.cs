0d61e6b507d7f799e7f6c0f79d6fccda650a326f
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2021-07-10 18:59:37 +0100

    add local things to PlayerInfo

--- QSB/Player/PlayerInfo.cs
+++ QSB/Player/PlayerInfo.cs
@@ -6,6 +6,7 @@ using QSB.ProbeSync;
 using QSB.QuantumSync;
 using QSB.RoastingSync;
 using QSB.Tools;
+using QSB.Utility;
 using System.Linq;
 using UnityEngine;
 
@@ -56,6 +57,64 @@ namespace QSB.Player
 		public bool IsInShrine; // TODO : move into PlayerStates?
 		public IQSBQuantumObject EntangledObject;
 		public bool IsDead { get; set; }
+
+		// Local only
+		public PlayerProbeLauncher LocalProbeLauncher
+		{
+			get
+			{
+				if (QSBPlayerManager.LocalPlayer != this)
+				{
+					DebugLog.ToConsole($"Warning - Tried to access local-only property LocalProbeLauncher in PlayerInfo for non local player!", OWML.Common.MessageType.Warning);
+					return null;
+				}
+
+				return CameraBody.transform.Find("ProbeLauncher").GetComponent<PlayerProbeLauncher>();
+			}
+		}
+
+		public Flashlight LocalFlashlight 
+		{
+			get
+			{
+				if (QSBPlayerManager.LocalPlayer != this)
+				{
+					DebugLog.ToConsole($"Warning - Tried to access local-only property LocalFlashlight in PlayerInfo for non local player!", OWML.Common.MessageType.Warning);
+					return null;
+				}
+
+				return Locator.GetFlashlight();
+			}
+		}
+
+		public Signalscope LocalSignalscope
+		{
+			get
+			{
+				if (QSBPlayerManager.LocalPlayer != this)
+				{
+					DebugLog.ToConsole($"Warning - Tried to access local-only property LocalSignalscope in PlayerInfo for non local player!", OWML.Common.MessageType.Warning);
+					return null;
+				}
+
+				return CameraBody.transform.Find("Signalscope").GetComponent<Signalscope>();
+			}
+		}
+
+		public NomaiTranslator LocalTranslator
+		{
+			get
+			{
+				if (QSBPlayerManager.LocalPlayer != this)
+				{
+					DebugLog.ToConsole($"Warning - Tried to access local-only property LocalTranslator in PlayerInfo for non local player!", OWML.Common.MessageType.Warning);
+					return null;
+				}
+
+				return CameraBody.transform.Find("NomaiTranslatorProp").GetComponent<NomaiTranslator>();
+			}
+		}
+
 
 		public PlayerInfo(uint id)
 		{
