fc01efd40726477c3b6cbab2b4e25d1a698c0551
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2021-08-14 14:45:36 +0100

    Create BaseUnsectoredSync.cs

---
+++ QSB/Syncs/Unsectored/BaseUnsectoredSync.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QSB.Syncs.Unsectored
+{
+	public abstract class BaseUnsectoredSync : SyncBase
+	{
+		public override bool IgnoreDisabledAttachedObject => false;
+		public override bool IgnoreNullReferenceTransform => false;
+		public override bool ShouldReparentAttachedObject => false;
+	}
+}
