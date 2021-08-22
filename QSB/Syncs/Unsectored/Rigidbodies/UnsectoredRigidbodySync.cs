776eadb3d96c1678649ad0fcf96504b15ca2a517
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2021-08-22 16:42:43 +0100

    add rigidbody sync (sectored and unsectored)

---
+++ QSB/Syncs/Unsectored/Rigidbodies/UnsectoredRigidbodySync.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace QSB.Syncs.Unsectored.Rigidbodies
+{
+	public abstract class UnsectoredRigidbodySync : BaseUnsectoredSync
+	{
+		protected abstract OWRigidbody GetRigidbody();
+
+		protected override Component SetAttachedObject()
+			=> throw new NotImplementedException();
+	}
+}
