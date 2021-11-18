15a973d109a037d664ca8735980333ae66057ae2
JohnCorby <williamforgames6@gmail.com>
2021-11-17 20:31:32 -0800

    fixed null bug in resync event

--- QSB/MeteorSync/WorldObjects/QSBFragment.cs
+++ QSB/MeteorSync/WorldObjects/QSBFragment.cs
@@ -1,4 +1,6 @@
-﻿using QSB.WorldSync;
+﻿using OWML.Common;
+using QSB.Utility;
+using QSB.WorldSync;
 using UnityEngine;
 
 namespace QSB.MeteorSync.WorldObjects
@@ -26,9 +28,20 @@ namespace QSB.MeteorSync.WorldObjects
 		public DetachableFragment DetachableFragment;
 		public bool IsThruWhiteHole => DetachableFragment != null &&
 			DetachableFragment._sector._parentSector == MeteorManager.WhiteHoleVolume._whiteHoleSector;
-		public OWRigidbody RefBody => IsThruWhiteHole ? MeteorManager.WhiteHoleVolume._whiteHoleBody :
-			Locator._brittleHollow._owRigidbody;
-		public OWRigidbody Body => IsThruWhiteHole ? AttachedObject.transform.parent.parent.GetAttachedOWRigidbody() : null;
+		public OWRigidbody RefBody => IsThruWhiteHole ? MeteorManager.WhiteHoleVolume._whiteHoleBody : Locator._brittleHollow._owRigidbody;
+		public OWRigidbody Body
+		{
+			get
+			{
+				if (IsThruWhiteHole)
+				{
+					return AttachedObject.transform.parent.parent.GetAttachedOWRigidbody();
+				}
+				DebugLog.ToConsole($"{LogName} - trying to get rigidbody when not thru white hole. "
+					+ "did you mean to get the transform instead?", MessageType.Error);
+				return null;
+			}
+		}
 
 		/// what the leash length will be when we eventually detach and fall thru white hole
 		public float LeashLength;
