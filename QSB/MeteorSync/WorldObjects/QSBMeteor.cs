161209535bb6230e55a515f4e01ba13cf65d342c
JohnCorby <williamforgames6@gmail.com>
2021-11-18 22:42:08 -0800

    we dont really need to transform sync meteors

--- QSB/MeteorSync/WorldObjects/QSBMeteor.cs
+++ QSB/MeteorSync/WorldObjects/QSBMeteor.cs
@@ -1,32 +1,14 @@
-﻿using QSB.MeteorSync.TransformSync;
-using QSB.Utility;
-using QSB.WorldSync;
-using QuantumUNET;
+﻿using QSB.WorldSync;
 using UnityEngine;
 
 namespace QSB.MeteorSync.WorldObjects
 {
 	public class QSBMeteor : WorldObject<MeteorController>
 	{
-		public MeteorTransformSync TransformSync;
-
 		public override void Init(MeteorController attachedObject, int id)
 		{
 			ObjectId = id;
 			AttachedObject = attachedObject;
-
-			if (QSBCore.IsHost)
-			{
-				Object.Instantiate(QSBNetworkManager.Instance.MeteorPrefab).SpawnWithServerAuthority();
-			}
-		}
-
-		public override void OnRemoval()
-		{
-			if (QSBCore.IsHost)
-			{
-				QNetworkServer.Destroy(TransformSync.gameObject);
-			}
 		}
 
 
