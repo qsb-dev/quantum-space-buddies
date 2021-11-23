9e893c8c5ca97a6ed57358a64edeb0cce8221cdc
JohnCorby <williamforgames6@gmail.com>
2021-11-23 02:06:56 -0800

    account for derived methods

--- QSB/Animation/NPC/Patches/TravelerControllerPatches.cs
+++ QSB/Animation/NPC/Patches/TravelerControllerPatches.cs
@@ -1,11 +1,6 @@
 ﻿using HarmonyLib;
 using QSB.Patches;
 using QSB.Utility;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace QSB.Animation.NPC.Patches
 {
@@ -21,13 +16,31 @@ namespace QSB.Animation.NPC.Patches
 			DebugLog.DebugWrite($"{__instance.name} OnStartConversation");
 			__instance._talking = true;
 
-			if (__instance._animator != null && __instance._animator.enabled)
+			if (__instance is GabbroTravelerController gabbro)
 			{
-				__instance._playingAnimID = __instance._animator.IsInTransition(0)
-					? __instance._animator.GetNextAnimatorStateInfo(0).fullPathHash
-					: __instance._animator.GetCurrentAnimatorStateInfo(0).fullPathHash;
+				if (gabbro._animator.enabled)
+				{
+					gabbro._animator.CrossFadeInFixedTime("Gabbro_Talking", 1.8f);
+					gabbro._hammockAnimator.CrossFadeInFixedTime("GabbroHammock_Talking", 1.8f);
+				}
+				// Locator.GetTravelerAudioManager().StopAllTravelerAudio();
+			}
+			else
+			{
+				if (__instance._animator != null && __instance._animator.enabled)
+				{
+					__instance._playingAnimID = __instance._animator.IsInTransition(0)
+						? __instance._animator.GetNextAnimatorStateInfo(0).fullPathHash
+						: __instance._animator.GetCurrentAnimatorStateInfo(0).fullPathHash;
 
-				__instance._animator.SetTrigger("Talking");
+					__instance._animator.SetTrigger("Talking");
+				}
+				// Locator.GetTravelerAudioManager().StopAllTravelerAudio();
+
+				if (__instance is ChertTravelerController chert)
+				{
+					chert._moodWeight = (float)chert._mood;
+				}
 			}
 
 			return false;
@@ -38,16 +51,37 @@ namespace QSB.Animation.NPC.Patches
 		public static bool OnEndConversation(TravelerController __instance)
 		{
 			DebugLog.DebugWrite($"{__instance.name} OnEndConversation");
-			if (__instance._animator != null && __instance._animator.enabled)
+
+			if (__instance is GabbroTravelerController gabbro)
 			{
-				if (__instance._delayToRestartAudio > 0f)
+				if (gabbro._animator.enabled)
 				{
-					__instance._animator.CrossFadeInFixedTime(__instance._playingAnimID, __instance._delayToRestartAudio, -1, -__instance._delayToRestartAudio);
+					gabbro._animator.CrossFadeInFixedTime("Gabbro_Playing", gabbro._delayToRestartAudio, -1, -gabbro._delayToRestartAudio);
+					gabbro._hammockAnimator.CrossFadeInFixedTime("GabbroHammock_Playing", gabbro._delayToRestartAudio, -1, -gabbro._delayToRestartAudio);
 				}
-				else
+				// Locator.GetTravelerAudioManager().PlayAllTravelerAudio(gabbro._delayToRestartAudio);
+				if (DialogueConditionManager.SharedInstance.GetConditionState("MAP_PROMPT_REMINDER") || DialogueConditionManager.SharedInstance.GetConditionState("MAP_PROMPT_ATTENTION"))
 				{
-					__instance._animator.SetTrigger("Playing");
+					var conditionState = DialogueConditionManager.SharedInstance.GetConditionState("MAP_PROMPT_ATTENTION");
+					DialogueConditionManager.SharedInstance.SetConditionState("MAP_PROMPT_REMINDER");
+					DialogueConditionManager.SharedInstance.SetConditionState("MAP_PROMPT_ATTENTION");
+					GlobalMessenger<bool>.FireEvent("TriggerMapPromptReminder", conditionState);
 				}
+			}
+			else
+			{
+				if (__instance._animator != null && __instance._animator.enabled)
+				{
+					if (__instance._delayToRestartAudio > 0f)
+					{
+						__instance._animator.CrossFadeInFixedTime(__instance._playingAnimID, __instance._delayToRestartAudio, -1, -__instance._delayToRestartAudio);
+					}
+					else
+					{
+						__instance._animator.SetTrigger("Playing");
+					}
+				}
+				// Locator.GetTravelerAudioManager().PlayAllTravelerAudio(__instance._delayToRestartAudio);
 			}
 
 			__instance._talking = false;
