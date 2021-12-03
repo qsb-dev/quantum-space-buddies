9428ff782608f6284746e323c512f9648ed47138
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2021-12-03 23:26:41 +0000

    add hearthian recorder sync

---
+++ QSB/Animation/NPC/WorldObjects/QSBHearthianRecorderEffects.cs
@@ -0,0 +1,11 @@
+﻿namespace QSB.Animation.NPC.WorldObjects
+{
+	internal class QSBHearthianRecorderEffects : NpcAnimController<HearthianRecorderEffects>
+	{
+		public override bool InConversation()
+			=> AttachedObject._characterDialogueTree.InConversation();
+
+		public override CharacterDialogueTree GetDialogueTree()
+			=> AttachedObject._characterDialogueTree;
+	}
+}
