589750649f753442e8e10809e9bb82dbbd1cb70e
JohnCorby <williamforgames6@gmail.com>
2021-12-12 03:01:41 -0800

    ShipDamageController_OnImpact NRE for real

--- QSB/ShipSync/Patches/ShipPatches.cs
+++ QSB/ShipSync/Patches/ShipPatches.cs
@@ -206,7 +206,14 @@ namespace QSB.ShipSync.Patches
 		[HarmonyPrefix]
 		[HarmonyPatch(typeof(ShipDamageController), nameof(ShipDamageController.OnImpact))]
 		public static bool ShipDamageController_OnImpact()
-			=> ShipManager.Instance && ShipManager.Instance.HasAuthority;
+		{
+			if (!ShipManager.Instance)
+			{
+				return true;
+			}
+
+			return ShipManager.Instance.HasAuthority;
+		}
 
 		[HarmonyPostfix]
 		[HarmonyPatch(typeof(ShipComponent), nameof(ShipComponent.RepairTick))]
