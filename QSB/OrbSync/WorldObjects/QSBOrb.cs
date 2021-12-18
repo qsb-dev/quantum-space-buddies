aa155f802bc9029348f93f4aa2270f36c1a05667
JohnCorby <williamforgames6@gmail.com>
2021-12-18 15:17:20 -0800

    initially spawn with no authority

--- QSB/OrbSync/WorldObjects/QSBOrb.cs
+++ QSB/OrbSync/WorldObjects/QSBOrb.cs
@@ -14,7 +14,7 @@ namespace QSB.OrbSync.WorldObjects
 		{
 			if (QSBCore.IsHost)
 			{
-				Object.Instantiate(QSBNetworkManager.Instance.OrbPrefab).SpawnWithServerAuthority();
+				QNetworkServer.Spawn(Object.Instantiate(QSBNetworkManager.Instance.OrbPrefab));
 			}
 
 			StartDelayedReady();
