c141c6bee5c134f4a4ded072f41f7ffc77e3984c
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2021-12-20 11:49:03 +0000

    improve server/client states

--- QSB/Player/Events/RequestStateResyncEvent.cs
+++ QSB/Player/Events/RequestStateResyncEvent.cs
@@ -62,7 +62,7 @@ namespace QSB.Player.Events
 				// if host, send worldobject and server states
 				if (isHost)
 				{
-					QSBEventManager.FireEvent(EventNames.QSBServerState, ServerStateManager.Instance.GetServerState());
+					ServerStateManager.Instance.FireChangeServerStateEvent(ServerStateManager.Instance.GetServerState());
 					QSBEventManager.FireEvent(EventNames.QSBPlayerInformation);
 
 					if (WorldObjectManager.AllObjectsReady)
