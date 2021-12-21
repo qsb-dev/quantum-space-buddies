378bb221a6d72c7e4486be38ea68ca8048efef9d
JohnCorby <williamforgames6@gmail.com>
2021-12-21 13:28:15 -0800

    baba

--- QSB/CampfireSync/CampfireManager.cs
+++ QSB/CampfireSync/CampfireManager.cs
@@ -5,7 +5,6 @@ namespace QSB.CampfireSync
 {
 	internal class CampfireManager : WorldObjectManager
 	{
-		// is this needed for the campfire in the eye? or is that a special one?
 		public override WorldObjectType WorldObjectType => WorldObjectType.Both;
 
 		protected override void RebuildWorldObjects(OWScene scene)
