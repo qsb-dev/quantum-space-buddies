a0655b67ebda49b5e16ff567077b4247e0629851
JohnCorby <williamforgames6@gmail.com>
2021-12-23 12:38:14 -0800

    check if local player exists before sending

--- QSB/EyeOfTheUniverse/EyeStateSync/Events/EyeStateMessage.cs
+++ QSB/EyeOfTheUniverse/EyeStateSync/Events/EyeStateMessage.cs
@@ -1,14 +1,22 @@
 ﻿using QSB.Events;
 using QSB.Messaging;
 using QSB.Player;
+using QSB.Player.TransformSync;
 using QSB.WorldSync;
 
 namespace QSB.EyeOfTheUniverse.EyeStateSync.Events
 {
 	internal class EyeStateMessage : QSBEnumMessage<EyeState>
 	{
-		static EyeStateMessage() => GlobalMessenger<EyeState>.AddListener(EventNames.EyeStateChanged,
-			state => new EyeStateMessage(state).Send());
+		static EyeStateMessage() => GlobalMessenger<EyeState>.AddListener(EventNames.EyeStateChanged, Handler);
+
+		private static void Handler(EyeState state)
+		{
+			if (PlayerTransformSync.LocalInstance != null)
+			{
+				new EyeStateMessage(state).Send();
+			}
+		}
 
 
 		public EyeStateMessage(EyeState state) => Value = state;
