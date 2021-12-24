12b5ec28fe0a90bceaf254b074bb4b6db8bd9c4b
JohnCorby <williamforgames6@gmail.com>
2021-12-23 17:24:05 -0800

    PlayerTranslatorMessage

---
+++ QSB/Tools/TranslatorTool/Messages/PlayerTranslatorMessage.cs
@@ -0,0 +1,38 @@
+﻿using QSB.Events;
+using QSB.Messaging;
+using QSB.Player;
+using QSB.Player.TransformSync;
+
+namespace QSB.Tools.TranslatorTool.Messages
+{
+	public class PlayerTranslatorMessage : QSBBoolMessage
+	{
+		static PlayerTranslatorMessage()
+		{
+			GlobalMessenger.AddListener(EventNames.EquipTranslator, () => Handle(true));
+			GlobalMessenger.AddListener(EventNames.UnequipTranslator, () => Handle(false));
+		}
+
+		private static void Handle(bool equipped)
+		{
+			if (PlayerTransformSync.LocalInstance != null)
+			{
+				new PlayerTranslatorMessage(equipped).Send();
+			}
+		}
+
+		private PlayerTranslatorMessage(bool equipped) => Value = equipped;
+
+		public PlayerTranslatorMessage() { }
+
+		public override void OnReceiveRemote()
+		{
+			var player = QSBPlayerManager.GetPlayer(From);
+			player.TranslatorEquipped = Value;
+			player.Translator?.ChangeEquipState(Value);
+		}
+
+		public override void OnReceiveLocal() =>
+			QSBPlayerManager.LocalPlayer.TranslatorEquipped = Value;
+	}
+}
