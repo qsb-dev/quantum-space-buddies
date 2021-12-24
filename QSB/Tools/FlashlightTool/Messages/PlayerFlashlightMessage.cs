ba0353fa56e60dac274a0d4aefafdc07ee28d9ac
JohnCorby <williamforgames6@gmail.com>
2021-12-23 18:55:22 -0800

    flashlight and signalscope

---
+++ QSB/Tools/FlashlightTool/Messages/PlayerFlashlightMessage.cs
@@ -0,0 +1,38 @@
+﻿using QSB.Events;
+using QSB.Messaging;
+using QSB.Player;
+using QSB.Player.TransformSync;
+
+namespace QSB.Tools.FlashlightTool.Messages
+{
+	public class PlayerFlashlightMessage : QSBBoolMessage
+	{
+		static PlayerFlashlightMessage()
+		{
+			GlobalMessenger.AddListener(EventNames.TurnOnFlashlight, () => Handle(true));
+			GlobalMessenger.AddListener(EventNames.TurnOffFlashlight, () => Handle(false));
+		}
+
+		private static void Handle(bool on)
+		{
+			if (PlayerTransformSync.LocalInstance != null)
+			{
+				new PlayerFlashlightMessage(on).Send();
+			}
+		}
+
+		private PlayerFlashlightMessage(bool on) => Value = on;
+
+		public PlayerFlashlightMessage() { }
+
+		public override void OnReceiveRemote()
+		{
+			var player = QSBPlayerManager.GetPlayer(From);
+			player.FlashlightActive = Value;
+			player.FlashLight?.UpdateState(Value);
+		}
+
+		public override void OnReceiveLocal() =>
+			QSBPlayerManager.LocalPlayer.FlashlightActive = Value;
+	}
+}
---
+++ QSB/Tools/SignalscopeTool/Messages/PlayerSignalscopeMessage.cs
@@ -0,0 +1,38 @@
+﻿using QSB.Events;
+using QSB.Messaging;
+using QSB.Player;
+using QSB.Player.TransformSync;
+
+namespace QSB.Tools.SignalscopeTool.Messages
+{
+	public class PlayerSignalscopeMessage : QSBBoolMessage
+	{
+		static PlayerSignalscopeMessage()
+		{
+			GlobalMessenger<Signalscope>.AddListener(EventNames.EquipSignalscope, _ => Handle(true));
+			GlobalMessenger.AddListener(EventNames.UnequipSignalscope, () => Handle(false));
+		}
+
+		private static void Handle(bool equipped)
+		{
+			if (PlayerTransformSync.LocalInstance != null)
+			{
+				new PlayerSignalscopeMessage(equipped).Send();
+			}
+		}
+
+		private PlayerSignalscopeMessage(bool equipped) => Value = equipped;
+
+		public PlayerSignalscopeMessage() { }
+
+		public override void OnReceiveRemote()
+		{
+			var player = QSBPlayerManager.GetPlayer(From);
+			player.SignalscopeEquipped = Value;
+			player.Signalscope?.ChangeEquipState(Value);
+		}
+
+		public override void OnReceiveLocal() =>
+			QSBPlayerManager.LocalPlayer.SignalscopeEquipped = Value;
+	}
+}
