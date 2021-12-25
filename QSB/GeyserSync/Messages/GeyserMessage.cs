a7e7b5e825944f6ed1b486246f031defe1bff805
JohnCorby <williamforgames6@gmail.com>
2021-12-24 21:23:40 -0800

    geyser

---
+++ QSB/GeyserSync/Messages/GeyserMessage.cs
@@ -0,0 +1,14 @@
+﻿using QSB.GeyserSync.WorldObjects;
+using QSB.Messaging;
+
+namespace QSB.GeyserSync.Messages
+{
+	public class GeyserMessage : QSBBoolWorldObjectMessage<QSBGeyser>
+	{
+		public GeyserMessage(bool state) => Value = state;
+
+		public GeyserMessage() { }
+
+		public override void OnReceiveRemote() => WorldObject.SetState(Value);
+	}
+}
