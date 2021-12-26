add598a7b4b0eaf4868782cdc135cd988037cd2f
JohnCorby <williamforgames6@gmail.com>
2021-12-26 02:58:24 -0800

    remove globalmessengers and callbacks

--- QSB/Utility/CustomCallbacks.cs
+++
@@ -1,3 +0,0 @@
-﻿public delegate void Callback<T, U, V, W>(T arg1, U arg2, V arg3, W arg4);
-public delegate void Callback<T, U, V, W, X>(T arg1, U arg2, V arg3, W arg4, X arg5);
-public delegate void Callback<T, U, V, W, X, Y>(T arg1, U arg2, V arg3, W arg4, X arg5, Y arg6);
