0d929d38b0e453ed3e516b6b69d3e1081aa6ae22
JohnCorby <williamforgames6@gmail.com>
2021-12-28 04:57:24 -0800

    weaver program

--- MirrorWeaver/Program.cs
+++ MirrorWeaver/Program.cs
@@ -26,66 +26,24 @@ namespace MirrorWeaver
 		}
 	}
 
-	internal static class Program
+	public static class Program
 	{
-		private static void Main(string[] args)
+		public static void Main(string[] args)
 		{
 			var log = new ConsoleLogger();
 			var weaver = new Weaver(log);
 
+			var qsbDll = args[0];
+			if (!File.Exists(qsbDll))
+			{
+				throw new Exception($"qsb dll could not be located at {qsbDll}!");
+			}
+
 			Console.WriteLine("Start weaving process.");
-
-			if (args.Length == 0)
-			{
-				log.Error("No args supplied!");
-			}
-
-			var unityEngine = args[0];
-			var qnetDLL = args[1];
-			var unetDLL = args[2];
-			var outputDirectory = args[3];
-			var assembly = args[4];
-
-			CheckDLLPath(unityEngine);
-			CheckDLLPath(qnetDLL);
-			CheckDLLPath(unetDLL);
-			CheckOutputDirectory(outputDirectory);
-			CheckAssemblyPath(assembly);
-			// weaver.WeaveAssemblies(assembly, null, null, outputDirectory, unityEngine, qnetDLL, unetDLL);
-			weaver.Weave(null, null, out _);
-		}
-
-		private static void CheckDLLPath(string path)
-		{
-			Console.WriteLine($"Check dll {path} ...");
-			if (!File.Exists(path))
-			{
-				throw new Exception("dll could not be located at " + path + "!");
-			}
-
-			Console.WriteLine("Path OK!");
-		}
-
-		private static void CheckAssemblyPath(string assemblyPath)
-		{
-			Console.WriteLine($"Check assembly path {assemblyPath} ...");
-			if (!File.Exists(assemblyPath))
-			{
-				throw new Exception("Assembly " + assemblyPath + " does not exist!");
-			}
-
-			Console.WriteLine("Assembly Path OK!");
-		}
-
-		private static void CheckOutputDirectory(string outputDir)
-		{
-			Console.WriteLine($"Check output path {outputDir} ...");
-			if (!Directory.Exists(outputDir))
-			{
-				Directory.CreateDirectory(outputDir);
-			}
-
-			Console.WriteLine("Output Path OK!");
+			var assembly = AssemblyDefinition.ReadAssembly(qsbDll);
+			weaver.Weave(assembly, new DefaultAssemblyResolver(), out var modified);
+			assembly.Write(new WriterParameters { WriteSymbols = true });
+			Console.WriteLine($"Finish weaving process. modified = {modified}");
 		}
 	}
 }
