b93ece8bd84f42684e522cc678b191046130d746
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2021-12-30 23:34:02 +0000

    bump version

---
+++ QSB/EyeOfTheUniverse/CosmicInflation/Patches/InflationPatches.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using QSB.Patches;
+using QSB.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QSB.EyeOfTheUniverse.CosmicInflation.Patches
+{
+	internal class InflationPatches : QSBPatch
+	{
+		public override QSBPatchTypes Type => QSBPatchTypes.OnClientConnect;
+
+		[HarmonyPrefix]
+		[HarmonyPatch(typeof(CosmicInflationController), nameof(CosmicInflationController.StartCollapse))]
+		public static bool StartCollapse(CosmicInflationController __instance)
+		{
+			DebugLog.DebugWrite($"Start Collapse");
+			return true;
+		}
+	}
+}
