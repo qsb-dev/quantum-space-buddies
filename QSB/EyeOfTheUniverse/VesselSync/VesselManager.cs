2725119024aaf421a611d8a234fd0b7e5474cbe0
JohnCorby <williamforgames6@gmail.com>
2021-12-29 22:34:00 -0800

    we dont need a world object for the vessel warp controller

--- QSB/EyeOfTheUniverse/VesselSync/VesselManager.cs
+++ QSB/EyeOfTheUniverse/VesselSync/VesselManager.cs
@@ -1,9 +1,8 @@
-﻿using QSB.EyeOfTheUniverse.VesselSync.WorldObjects;
+﻿using QSB.Messaging;
 using QSB.Player;
-using QSB.Utility;
+using QSB.Player.Messages;
 using QSB.WorldSync;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 namespace QSB.EyeOfTheUniverse.VesselSync
@@ -12,8 +11,8 @@ namespace QSB.EyeOfTheUniverse.VesselSync
 	{
 		public static VesselManager Instance { get; private set; }
 
-		private List<PlayerInfo> _playersInCage = new();
-		private QSBVesselWarpController _warpController;
+		private readonly List<PlayerInfo> _playersInCage = new();
+		private VesselWarpController _warpController;
 
 		public override WorldObjectType WorldObjectType => WorldObjectType.Both;
 
@@ -25,10 +24,35 @@ namespace QSB.EyeOfTheUniverse.VesselSync
 
 		protected override void RebuildWorldObjects(OWScene scene)
 		{
-			QSBWorldSync.Init<QSBVesselWarpController, VesselWarpController>();
-			_warpController = QSBWorldSync.GetWorldObjects<QSBVesselWarpController>().First();
-			_warpController.AttachedObject._cageTrigger.OnExit -= _warpController.AttachedObject.OnExitCageTrigger;
+			if (_warpController != null)
+			{
+				_warpController._cageTrigger.OnEntry -= OnEntry;
+				_warpController._cageTrigger.OnExit -= OnExit;
+			}
+
+			_warpController = FindObjectOfType<VesselWarpController>();
+			_warpController._cageTrigger.OnExit -= _warpController.OnExitCageTrigger;
+
+			_warpController._cageTrigger.OnEntry += OnEntry;
+			_warpController._cageTrigger.OnExit += OnExit;
 		}
+
+		private static void OnEntry(GameObject hitObj)
+		{
+			if (hitObj.CompareTag("PlayerDetector"))
+			{
+				new EnterLeaveMessage(EnterLeaveType.EnterVesselCage).Send();
+			}
+		}
+
+		private static void OnExit(GameObject hitObj)
+		{
+			if (hitObj.CompareTag("PlayerDetector"))
+			{
+				new EnterLeaveMessage(EnterLeaveType.ExitVesselCage).Send();
+			}
+		}
+
 
 		public void Enter(PlayerInfo player)
 		{
@@ -39,15 +63,15 @@ namespace QSB.EyeOfTheUniverse.VesselSync
 		{
 			_playersInCage.Remove(player);
 
-			if (_playersInCage.Count == 0 && _warpController.AttachedObject._hasPower)
+			if (_playersInCage.Count == 0 && _warpController._hasPower)
 			{
-				var obj = _warpController.AttachedObject;
+				var obj = _warpController;
 				obj._cageClosed = true;
 				obj._cageAnimator.TranslateToLocalPosition(new Vector3(0f, -8.1f, 0f), 5f);
 				obj._cageAnimator.RotateToLocalEulerAngles(new Vector3(0f, 180f, 0f), 5f);
 				obj._cageAnimator.OnTranslationComplete -= obj.OnCageAnimationComplete;
 				obj._cageAnimator.OnTranslationComplete += obj.OnCageAnimationComplete;
-				obj._cageLoopingAudio.FadeIn(1f, false, false, 1f);
+				obj._cageLoopingAudio.FadeIn(1f);
 			}
 		}
 	}
