1780334051d475f1df4a50f09d88e30a2497cdcf
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2021-12-30 16:06:10 +0000

    changed to IntoTheVortex and above

--- QSB/Player/Messages/PlayerJoinMessage.cs
+++ QSB/Player/Messages/PlayerJoinMessage.cs
@@ -75,7 +75,7 @@ namespace QSB.Player.Messages
 					return;
 				}
 
-				if (QSBPlayerManager.PlayerList.Any(x => x.EyeState > EyeState.Observatory))
+				if (QSBPlayerManager.PlayerList.Any(x => x.EyeState > EyeState.WarpedToSurface))
 				{
 					DebugLog.ToConsole($"Error - Client {PlayerName} connecting too late into eye scene.", MessageType.Error);
 					new PlayerKickMessage(From, KickReason.InEye).Send();
