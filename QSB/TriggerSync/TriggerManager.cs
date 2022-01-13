066df2ff3fd48213fe040db364203b8b89be151e
JohnCorby <williamforgames6@gmail.com>
2022-01-12 17:02:21 -0800

    trigger

---
+++ QSB/TriggerSync/TriggerManager.cs
@@ -0,0 +1,32 @@
+﻿using OWML.Common;
+using QSB.Utility;
+using QSB.WorldSync;
+using System.Linq;
+
+namespace QSB.TriggerSync
+{
+	public class TriggerManager : WorldObjectManager
+	{
+		public override WorldObjectType WorldObjectType => WorldObjectType.Both;
+
+		private static TriggerLink[] _triggerLinks;
+
+		protected override void RebuildWorldObjects(OWScene scene)
+		{
+			_triggerLinks?.ForEach(x => x.Dispose());
+			_triggerLinks = QSBWorldSync.GetUnityObjects<OWTriggerVolume>()
+				.Select((x, i) => new TriggerLink(i, x))
+				.ToArray();
+		}
+
+		public static TriggerLink GetTriggerLink(int id)
+		{
+			if (!_triggerLinks.TryGet(id, out var triggerLink))
+			{
+				DebugLog.ToConsole($"no trigger link with id {id}", MessageType.Error);
+			}
+
+			return triggerLink;
+		}
+	}
+}
---
+++ QSB/TriggerSync/TriggerMessage.cs
@@ -0,0 +1,48 @@
+﻿using QSB.Messaging;
+using QSB.Player;
+using QSB.WorldSync;
+using QuantumUNET.Transport;
+
+namespace QSB.TriggerSync
+{
+	public class TriggerMessage : QSBBoolMessage
+	{
+		private int _id;
+
+		public TriggerMessage(int id, bool entered)
+		{
+			_id = id;
+			Value = entered;
+		}
+
+		public override void Serialize(QNetworkWriter writer)
+		{
+			base.Serialize(writer);
+			writer.Write(_id);
+		}
+
+		public override void Deserialize(QNetworkReader reader)
+		{
+			base.Deserialize(reader);
+			_id = reader.ReadInt32();
+		}
+
+		public override bool ShouldReceive => WorldObjectManager.AllObjectsReady;
+
+		public override void OnReceiveLocal() => OnReceiveRemote();
+
+		public override void OnReceiveRemote()
+		{
+			var player = QSBPlayerManager.GetPlayer(From);
+			var triggerLink = TriggerManager.GetTriggerLink(_id);
+			if (Value)
+			{
+				triggerLink.Enter(player);
+			}
+			else
+			{
+				triggerLink.Exit(player);
+			}
+		}
+	}
+}
