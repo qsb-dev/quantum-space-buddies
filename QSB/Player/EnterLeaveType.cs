20c222d281c7cf9ddfe15d5649e5aea4a8473651
JohnCorby <williamforgames6@gmail.com>
2022-01-12 21:43:57 -0800

    mask zone

--- QSB/Player/EnterLeaveType.cs
+++ QSB/Player/EnterLeaveType.cs
@@ -8,7 +8,5 @@
 		ExitPlatform,
 		EnterShip,
 		ExitShip,
-		EnterMaskZone,
-		ExitMaskZone
 	}
 }
--- QSB/TriggerSync/WorldObjects/QSBMaskZoneTrigger.cs
+++ QSB/TriggerSync/WorldObjects/QSBMaskZoneTrigger.cs
@@ -1,5 +1,4 @@
-﻿using QSB.EyeOfTheUniverse.MaskSync;
-using QSB.Player;
+﻿using QSB.Player;
 
 namespace QSB.TriggerSync.WorldObjects
 {
@@ -12,8 +11,28 @@ namespace QSB.TriggerSync.WorldObjects
 			AttachedObject.OnExit -= TriggerOwner.OnExitMaskZone;
 		}
 
-		protected override void OnEnter(PlayerInfo player) => MaskManager.Instance.Enter(player);
+		protected override void OnEnter(PlayerInfo player)
+		{
+			if (Occupants.Count == 1)
+			{
+				TriggerOwner._whiteSphere.SetActive(true);
+				TriggerOwner._groundSignal.SetSignalActivation(false);
+				TriggerOwner._skySignal.SetSignalActivation(true);
+				TriggerOwner._skeletonTower.SetIsQuantum(TriggerOwner._hasPlayerLookedAtSky);
+				TriggerOwner.enabled = true;
+			}
+		}
 
-		protected override void OnExit(PlayerInfo player) => MaskManager.Instance.Exit(player);
+		protected override void OnExit(PlayerInfo player)
+		{
+			if (Occupants.Count == 0 && !TriggerOwner._shuttle.HasLaunched())
+			{
+				TriggerOwner._whiteSphere.SetActive(false);
+				TriggerOwner._skeletonTower.SetIsQuantum(false);
+				TriggerOwner._groundSignal.SetSignalActivation(true);
+				TriggerOwner._skySignal.SetSignalActivation(false);
+				TriggerOwner.enabled = false;
+			}
+		}
 	}
 }
