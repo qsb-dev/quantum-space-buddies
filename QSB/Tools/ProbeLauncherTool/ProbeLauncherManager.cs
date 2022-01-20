44b1a8714eb83f9ae7e1dcf88e467b767e5fe62b
JohnCorby <williamforgames6@gmail.com>
2022-01-20 13:22:58 -0800

    ship probe launcher

--- QSB/Tools/ProbeLauncherTool/ProbeLauncherManager.cs
+++ QSB/Tools/ProbeLauncherTool/ProbeLauncherManager.cs
@@ -1,5 +1,6 @@
 ﻿using QSB.Tools.ProbeLauncherTool.WorldObjects;
 using QSB.WorldSync;
+using System.Linq;
 
 namespace QSB.Tools.ProbeLauncherTool
 {
@@ -8,6 +9,12 @@ namespace QSB.Tools.ProbeLauncherTool
 		public override WorldObjectType WorldObjectType => WorldObjectType.Both;
 
 		public override void BuildWorldObjects(OWScene scene)
-			=> QSBWorldSync.Init<QSBProbeLauncher, ProbeLauncher>(typeof(PlayerProbeLauncher));
+		{
+			QSBWorldSync.Init<QSBProbeLauncher, ProbeLauncher>(typeof(PlayerProbeLauncher));
+			QSBWorldSync.Init<QSBProbeLauncher, ProbeLauncher>(new[]
+			{
+				QSBWorldSync.GetUnityObjects<ShipCockpitController>().First().GetShipProbeLauncher()
+			});
+		}
 	}
 }
