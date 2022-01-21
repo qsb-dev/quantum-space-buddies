840ce8119e6c7599bc9117d3e803bd4e7dc01941
JohnCorby <williamforgames6@gmail.com>
2022-01-20 22:41:21 -0800

    dont move probe back to launcher when it's retrieved. it actually looks better this way for non-player-launchers

--- QSB/Syncs/SyncBase.cs
+++ QSB/Syncs/SyncBase.cs
@@ -70,7 +70,6 @@ namespace QSB.Syncs
 
 		protected abstract bool IsReady { get; }
 		protected abstract bool UseInterpolation { get; }
-		protected virtual bool AllowDisabledAttachedObject => false;
 		protected abstract bool AllowNullReferenceTransform { get; }
 		protected virtual bool IsPlayerObject => false;
 		protected virtual bool OnlyApplyOnDeserialize => false;
@@ -177,7 +176,7 @@ namespace QSB.Syncs
 				return;
 			}
 
-			if (!AttachedTransform.gameObject.activeInHierarchy && !AllowDisabledAttachedObject)
+			if (!AttachedTransform.gameObject.activeInHierarchy)
 			{
 				return;
 			}
