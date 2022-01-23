4290a3b1636a3caba414a3f552a5e46db345e07b
JohnCorby <williamforgames6@gmail.com>
2022-01-23 00:19:43 -0800

    simplify elevator

--- QSB/ElevatorSync/WorldObjects/QSBElevator.cs
+++ QSB/ElevatorSync/WorldObjects/QSBElevator.cs
@@ -7,36 +7,23 @@ namespace QSB.ElevatorSync.WorldObjects
 {
 	public class QSBElevator : WorldObject<Elevator>
 	{
-		private Vector3 _startLocalPos;
-		private Vector3 _endLocalPos;
-
-		private SingleInteractionVolume _interactVolume;
-		private OWAudioSource _owAudioSourceOneShot;
-		private OWAudioSource _owAudioSourceLP;
 		private OWTriggerVolume _elevatorTrigger;
 
 		public override void Init()
 		{
 			StartDelayedReady();
-			QSBCore.UnityEvents.RunWhen(() => AttachedObject._interactVolume != null, InitValues);
-		}
+			QSBCore.UnityEvents.RunWhen(() => AttachedObject._interactVolume, () =>
+			{
+				FinishDelayedReady();
 
-		private void InitValues()
-		{
-			FinishDelayedReady();
-			_startLocalPos = AttachedObject._startLocalPos;
-			_endLocalPos = AttachedObject._endLocalPos;
-			_interactVolume = AttachedObject._interactVolume;
-			_owAudioSourceOneShot = AttachedObject._owAudioSourceOneShot;
-			_owAudioSourceLP = AttachedObject._owAudioSourceLP;
+				// BUG : This won't work for the log lift! need to make a different trigger for that
 
-			// BUG : This won't work for the log lift! need to make a different trigger for that
+				var boxShape = AttachedObject.gameObject.GetAddComponent<BoxShape>();
+				boxShape.center = new Vector3(0, 1.75f, 0.25f);
+				boxShape.size = new Vector3(3, 3.5f, 3);
 
-			var boxShape = AttachedObject.gameObject.GetAddComponent<BoxShape>();
-			boxShape.center = new Vector3(0, 1.75f, 0.25f);
-			boxShape.size = new Vector3(3, 3.5f, 3);
-
-			_elevatorTrigger = AttachedObject.gameObject.GetAddComponent<OWTriggerVolume>();
+				_elevatorTrigger = AttachedObject.gameObject.GetAddComponent<OWTriggerVolume>();
+			});
 		}
 
 		public override void SendResyncInfo(uint to)
@@ -60,31 +47,20 @@ namespace QSB.ElevatorSync.WorldObjects
 					Locator.GetPlayerSuit().RemoveSuit();
 				}
 
-				RemoteStartLift();
+				AttachedObject.StartLift();
 			}
 			else
 			{
 				SetDirection(isGoingUp);
-				RemoteStartLift();
+				AttachedObject.StartLift();
 			}
 		}
 
 		private void SetDirection(bool isGoingUp)
 		{
-			_interactVolume.transform.Rotate(0f, 180f, 0f);
-			var targetPos = isGoingUp ? _endLocalPos : _startLocalPos;
-			AttachedObject._targetLocalPos = targetPos;
+			AttachedObject._interactVolume.transform.Rotate(0f, 180f, 0f);
 			AttachedObject._goingToTheEnd = isGoingUp;
-		}
-
-		private void RemoteStartLift()
-		{
-			AttachedObject.enabled = true;
-			AttachedObject._initLocalPos = AttachedObject.transform.localPosition;
-			AttachedObject._initLiftTime = Time.time;
-			_owAudioSourceOneShot.PlayOneShot(AudioType.TH_LiftActivate);
-			_owAudioSourceLP.FadeIn(0.5f);
-			_interactVolume.DisableInteraction();
+			AttachedObject._targetLocalPos = isGoingUp ? AttachedObject._endLocalPos : AttachedObject._startLocalPos;
 		}
 
 		public override void DisplayLines()
