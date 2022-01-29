4c1e0ba3d6f5b23a15b32e805a28e22d7ae113ae
JohnCorby <williamforgames6@gmail.com>
2022-01-29 01:59:58 -0800

    all things in Delay should perform their checks/do their things at the very end of the PlayerLoop

--- QSB/Utility/Delay.cs
+++ QSB/Utility/Delay.cs
@@ -7,19 +7,19 @@ namespace QSB.Utility
 	{
 		public static void RunNextFrame(Action action) => UniTask.Create(async () =>
 		{
-			await UniTask.NextFrame();
+			await UniTask.NextFrame(PlayerLoopTiming.LastPostLateUpdate);
 			action();
 		});
 
 		public static void RunFramesLater(int n, Action action) => UniTask.Create(async () =>
 		{
-			await UniTask.DelayFrame(n);
+			await UniTask.DelayFrame(n, PlayerLoopTiming.LastPostLateUpdate);
 			action();
 		});
 
 		public static void RunWhen(Func<bool> predicate, Action action) => UniTask.Create(async () =>
 		{
-			await UniTask.WaitUntil(predicate);
+			await UniTask.WaitUntil(predicate, PlayerLoopTiming.LastPostLateUpdate);
 			action();
 		});
 	}
