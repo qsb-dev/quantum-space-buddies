37272f0a100351608e22659498dd0b62ce8322a0
JohnCorby <williamforgames6@gmail.com>
2022-01-29 01:23:29 -0800

    UnityEvents using UniTask

---
+++ QSB/Utility/UnityEvents.cs
@@ -0,0 +1,44 @@
+﻿using Cysharp.Threading.Tasks;
+using System;
+
+namespace QSB.Utility
+{
+	public static class UnityEvents
+	{
+		public static UniTask FireOnNextUpdate(Action action) => UniTask.Create(async () =>
+		{
+			await UniTask.NextFrame();
+			action();
+		});
+
+		public static UniTask FireInNUpdates(Action action, int n) => UniTask.Create(async () =>
+		{
+			await UniTask.DelayFrame(n);
+			action();
+		});
+
+		public static UniTask RunWhen(Func<bool> predicate, Action action) => UniTask.Create(async () =>
+		{
+			await UniTask.WaitUntil(predicate);
+			action();
+		});
+
+		public static UniTask FireOnNextUpdate(Func<UniTask> func) => UniTask.Create(async () =>
+		{
+			await UniTask.NextFrame();
+			await func();
+		});
+
+		public static UniTask FireInNUpdates(Func<UniTask> func, int n) => UniTask.Create(async () =>
+		{
+			await UniTask.DelayFrame(n);
+			await func();
+		});
+
+		public static UniTask RunWhen(Func<bool> predicate, Func<UniTask> func) => UniTask.Create(async () =>
+		{
+			await UniTask.WaitUntil(predicate);
+			await func();
+		});
+	}
+}
