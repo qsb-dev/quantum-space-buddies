e657c8a48aaaaceef1bfcba3141261447d3e804e
JohnCorby <williamforgames6@gmail.com>
2022-01-29 01:31:05 -0800

    cancellation support

--- QSB/Utility/Delay.cs
+++ QSB/Utility/Delay.cs
@@ -1,5 +1,6 @@
 ﻿using Cysharp.Threading.Tasks;
 using System;
+using System.Threading;
 
 namespace QSB.Utility
 {
@@ -38,6 +39,42 @@ namespace QSB.Utility
 		public static UniTask RunWhen(Func<bool> predicate, Func<UniTask> func) => UniTask.Create(async () =>
 		{
 			await UniTask.WaitUntil(predicate);
+			await func();
+		});
+
+		public static UniTask RunNextFrame(Action action, CancellationToken ct) => UniTask.Create(async () =>
+		{
+			await UniTask.NextFrame(ct);
+			action();
+		});
+
+		public static UniTask RunFramesLater(int n, Action action, CancellationToken ct) => UniTask.Create(async () =>
+		{
+			await UniTask.DelayFrame(n, cancellationToken: ct);
+			action();
+		});
+
+		public static UniTask RunWhen(Func<bool> predicate, Action action, CancellationToken ct) => UniTask.Create(async () =>
+		{
+			await UniTask.WaitUntil(predicate, cancellationToken: ct);
+			action();
+		});
+
+		public static UniTask RunNextFrame(Func<UniTask> func, CancellationToken ct) => UniTask.Create(async () =>
+		{
+			await UniTask.NextFrame(ct);
+			await func();
+		});
+
+		public static UniTask RunFramesLater(int n, Func<UniTask> func, CancellationToken ct) => UniTask.Create(async () =>
+		{
+			await UniTask.DelayFrame(n, cancellationToken: ct);
+			await func();
+		});
+
+		public static UniTask RunWhen(Func<bool> predicate, Func<UniTask> func, CancellationToken ct) => UniTask.Create(async () =>
+		{
+			await UniTask.WaitUntil(predicate, cancellationToken: ct);
 			await func();
 		});
 	}
