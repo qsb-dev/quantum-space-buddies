f7ed57a787ffdc8ba0b11f2e4cdf0ef494e07045
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2022-01-31 19:33:55 +0000

    Revert "Revert "cleanup fizzyfacepunch""
    This reverts commit c049b94527a16c3ae0f04c644362cf5b3f3f114b.

--- FizzyFacepunch/FizzyConnectionManager.cs
+++ FizzyFacepunch/FizzyConnectionManager.cs
@@ -3,10 +3,7 @@ using System;
 
 public class FizzyConnectionManager : ConnectionManager
 {
-    public Action<IntPtr, int> ForwardMessage;
+	public Action<IntPtr, int> ForwardMessage;
 
-    public override void OnMessage(IntPtr data, int size, long messageNum, long recvTime, int channel)
-    {
-        ForwardMessage(data, size);
-    }
+	public override void OnMessage(IntPtr data, int size, long messageNum, long recvTime, int channel) => ForwardMessage(data, size);
 }
