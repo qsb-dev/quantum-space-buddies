1783271c0fcd2ee20c5ca54a5d07187a3864ef8a
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2022-02-05 10:14:27 +0000

    Fix collision errors when loading

--- QSB/TimeSync/Patches/TimePatches.cs
+++ QSB/TimeSync/Patches/TimePatches.cs
@@ -16,10 +16,14 @@ namespace QSB.TimeSync.Patches
 			DebugLog.DebugWrite($"OnStartOfTimeLoop");
 			return false;
 		}
+
 		[HarmonyPrefix]
 		[HarmonyPatch(typeof(OWTime), nameof(OWTime.Pause))]
-		public static bool StopPausing()
-			=> false;
+		public static bool StopPausing(OWTime.PauseType pauseType)
+			=> pauseType
+			is OWTime.PauseType.Initializing
+			or OWTime.PauseType.Streaming
+			or OWTime.PauseType.Loading;
 
 		[HarmonyPrefix]
 		[HarmonyPatch(typeof(SubmitActionSkipToNextLoop), nameof(SubmitActionSkipToNextLoop.AdvanceToNewTimeLoop))]
