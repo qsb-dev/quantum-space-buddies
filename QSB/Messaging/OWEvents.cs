1a14aff60c1c02d26f11ba8a72eadd83f6d88c2e
JohnCorby <williamforgames6@gmail.com>
2022-02-05 04:06:55 -0800

    whoops

--- QSB/Messaging/OWEvents.cs
+++ QSB/Messaging/OWEvents.cs
@@ -29,6 +29,5 @@
 		public const string ExitShip = nameof(ExitShip);
 		public const string EyeStateChanged = nameof(EyeStateChanged);
 		public const string FlickerOffAndOn = nameof(FlickerOffAndOn);
-		public const string PlayerDeath = nameof(PlayerDeath);
 	}
 }
