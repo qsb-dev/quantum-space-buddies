b327b0c72737d507cf33122e383d326964ec57bc
JohnCorby <williamforgames6@gmail.com>
2022-02-06 21:52:18 -0800

    more work
    ownership will be passed via exit code

--- EpicRerouter/Interop.cs
+++ EpicRerouter/Interop.cs
@@ -1,17 +1,50 @@
 ﻿using HarmonyLib;
-using UnityEngine;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Debug = UnityEngine.Debug;
 
 namespace EpicRerouter
 {
 	public static class Interop
 	{
-		private static void Log(object msg) => Debug.LogError($"[interop] {msg}");
+		public static void Log(object msg) => Debug.LogError($"[interop] {msg}");
 
 		public static void Go()
 		{
 			Log("go");
 
 			Harmony.CreateAndPatchAll(typeof(Patches));
+
+			var processPath = Path.Combine(
+				Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!,
+				"EpicRerouter.exe"
+			);
+			var assemblyLocations = AppDomain.CurrentDomain.GetAssemblies()
+				.Select(x =>
+				{
+					try
+					{
+						return x.Location == string.Empty ? null : x.Location;
+					}
+					catch
+					{
+						return null;
+					}
+				})
+				.Where(x => x != null);
+			var process = Process.Start(new ProcessStartInfo
+			{
+				FileName = processPath,
+				WorkingDirectory = Path.GetDirectoryName(processPath)!,
+				Arguments = assemblyLocations.Join(x => $"\"{x}\"", " "),
+				UseShellExecute = false
+			});
+			process!.WaitForExit();
+			var ownershipStatus = (EntitlementsManager.AsyncOwnershipStatus)process.ExitCode;
+			Log($"ownership status = {ownershipStatus}");
 		}
 	}
 }
--- EpicRerouter/Program.cs
+++ EpicRerouter/Program.cs
@@ -1,12 +1,27 @@
-﻿namespace EpicRerouter
+﻿using Epic.OnlineServices.Ecom;
+using System;
+using System.Reflection;
+using System.Threading;
+
+namespace EpicRerouter
 {
 	public static class Program
 	{
-		public static void Main(string[] args)
+		public static void Main(string[] assemblyLocations)
 		{
-			foreach (var s in args)
+			try
 			{
-				
+				foreach (var assemblyLocation in assemblyLocations)
+				{
+					var assembly = Assembly.LoadFile(assemblyLocation);
+					Console.WriteLine($"loaded {assembly}");
+				}
+			}
+			finally
+			{
+				Console.WriteLine("all done");
+				Thread.Sleep(3000);
+				Environment.Exit((int)OwnershipStatus.Owned);
 			}
 		}
 	}
