a63e2bfb5ccc0794498c7c52e719c5ab06ab6e03
JohnCorby <williamforgames6@gmail.com>
2022-02-06 22:22:58 -0800

    add classes from game

---
+++ EpicRerouter/EpicEntitlementRetriever.cs
@@ -0,0 +1,67 @@
+﻿using Epic.OnlineServices;
+using Epic.OnlineServices.Ecom;
+using UnityEngine;
+using static EntitlementsManager;
+
+namespace EpicRerouter
+{
+	/// <summary>
+	/// runs on process side
+	/// </summary>
+	public static class EpicEntitlementRetriever
+	{
+		public const string EOS_DLC_OFFER_ID = "379b9688bb1048f3a56d52b13800f4df";
+		public const string EOS_DLC_ITEM_ID = "49a9ac61fe464cbf8c8c73f46b3f1133";
+
+		private static EcomInterface _ecomInterface;
+		private static OwnershipStatus _epicDlcOwnershipStatus;
+		private static bool _epicResultReceived;
+
+		public static void Init()
+		{
+			EpicPlatformManager.onAuthSuccess += EOSQueryOwnership;
+		}
+
+		public static void Uninit()
+		{
+			EpicPlatformManager.onAuthSuccess -= EOSQueryOwnership;
+		}
+
+		public static AsyncOwnershipStatus GetOwnershipStatus()
+		{
+			if (!_epicResultReceived)
+			{
+				return AsyncOwnershipStatus.NotReady;
+			}
+
+			if (_epicDlcOwnershipStatus != OwnershipStatus.Owned)
+			{
+				return AsyncOwnershipStatus.NotOwned;
+			}
+
+			return AsyncOwnershipStatus.Owned;
+		}
+
+		public static void EOSQueryOwnership()
+		{
+			Debug.Log("[EOS] querying DLC ownership");
+			_ecomInterface = EpicPlatformManager.PlatformInterface.GetEcomInterface();
+			var queryOwnershipOptions = new QueryOwnershipOptions
+			{
+				LocalUserId = EpicPlatformManager.LocalUserId,
+				CatalogItemIds = new[] { "49a9ac61fe464cbf8c8c73f46b3f1133" }
+			};
+			_ecomInterface.QueryOwnership(queryOwnershipOptions, null, OnEOSQueryOwnershipComplete);
+		}
+
+		private static void OnEOSQueryOwnershipComplete(QueryOwnershipCallbackInfo data)
+		{
+			if (data.ResultCode == Result.Success)
+			{
+				_epicDlcOwnershipStatus = data.ItemOwnership[0].OwnershipStatus;
+				_epicResultReceived = true;
+				Debug.Log($"[EOS] Query DLC ownership complete: {_epicDlcOwnershipStatus}");
+			}
+		}
+	}
+}
---
+++ EpicRerouter/EpicPlatformManager.cs
@@ -0,0 +1,146 @@
+﻿using Epic.OnlineServices;
+using Epic.OnlineServices.Auth;
+using Epic.OnlineServices.Platform;
+using System;
+using UnityEngine;
+
+namespace EpicRerouter
+{
+	/// <summary>
+	/// runs on process side
+	/// </summary>
+	public static class EpicPlatformManager
+	{
+		public const string EOS_PRODUCT_ID = "prod-starfish";
+		public const string EOS_SANDBOX_ID = "starfish";
+		public const string EOS_DEPLOYMENT_ID = "e176ecc84fbc4dd8934664684f44dc71";
+		public const string EOS_CLIENT_ID = "5c553c6accee4111bc8ea3a3ae52229b";
+		public const string EOS_CLIENT_SECRET = "k87Nfp75BzPref4nJFnnbNjYXQQR";
+
+		private const float TICK_INTERVAL = 0.1f;
+		private const bool ENABLE_SDK_DEBUGGING = false;
+
+		public static PlatformInterface PlatformInterface { get; private set; }
+		public static EpicAccountId LocalUserId { get; private set; }
+		private static float _lastTick;
+
+		public static OWEvent onAuthSuccess = new(1);
+
+		public static void Init()
+		{
+			if (PlatformInterface == null)
+			{
+				try
+				{
+					InitPlatform();
+				}
+				catch (EOSInitializeException ex)
+				{
+					if (ex.Result == Result.AlreadyConfigured)
+					{
+						Debug.Log("[EOS] platform already configured!");
+					}
+				}
+			}
+
+			Auth();
+		}
+
+		public static void Tick()
+		{
+			if (PlatformInterface != null && _lastTick + 0.1f <= Time.unscaledTime)
+			{
+				PlatformInterface.Tick();
+				_lastTick = Time.unscaledTime;
+			}
+		}
+
+		public static void Uninit()
+		{
+			PlatformInterface.Release();
+			PlatformInterface = null;
+			PlatformInterface.Shutdown();
+		}
+
+		private static void InitPlatform()
+		{
+			var result = PlatformInterface.Initialize(new InitializeOptions
+			{
+				ProductName = Application.productName,
+				ProductVersion = Application.version
+			});
+			if (result != Result.Success)
+			{
+				throw new EOSInitializeException("Failed to initialize Epic Online Services platform: ", result);
+			}
+
+			var options = new Options
+			{
+				ProductId = "prod-starfish",
+				SandboxId = "starfish",
+				ClientCredentials = new ClientCredentials
+				{
+					ClientId = "5c553c6accee4111bc8ea3a3ae52229b",
+					ClientSecret = "k87Nfp75BzPref4nJFnnbNjYXQQR"
+				},
+				DeploymentId = "e176ecc84fbc4dd8934664684f44dc71"
+			};
+			PlatformInterface = PlatformInterface.Create(options);
+			Debug.Log("[EOS] Platform interface has been created");
+		}
+
+		private static void Auth()
+		{
+			Debug.Log("[EOS] Authenticating...");
+			var loginOptions = new LoginOptions
+			{
+				Credentials = new Credentials
+				{
+					Type = LoginCredentialType.ExchangeCode,
+					Id = null,
+					Token = GetPasswordFromCommandLine()
+				},
+				ScopeFlags = 0
+			};
+			if (PlatformInterface == null)
+			{
+				Debug.LogError("[EOS] Platform interface is null!");
+			}
+
+			PlatformInterface.GetAuthInterface().Login(loginOptions, null, OnLogin);
+		}
+
+		private static string GetPasswordFromCommandLine()
+		{
+			var commandLineArgs = Environment.GetCommandLineArgs();
+			foreach (var arg in commandLineArgs)
+			{
+				if (arg.Contains("AUTH_PASSWORD"))
+				{
+					return arg.Split('=')[1];
+				}
+			}
+
+			return null;
+		}
+
+		private static void OnLogin(LoginCallbackInfo loginCallbackInfo)
+		{
+			if (loginCallbackInfo.ResultCode == Result.Success)
+			{
+				LocalUserId = loginCallbackInfo.LocalUserId;
+				Debug.Log($"[EOS SDK] login success! user ID: {LocalUserId}");
+				onAuthSuccess.Invoke();
+				return;
+			}
+
+			Debug.LogError("[EOS SDK] Login failed");
+		}
+
+		public class EOSInitializeException : Exception
+		{
+			public readonly Result Result;
+			public EOSInitializeException(string msg, Result initResult) : base(msg) => Result = initResult;
+		}
+	}
+}
