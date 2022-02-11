e38feb4f28043b1816a88068c3cf7463cd065c3e
JohnCorby <williamforgames6@gmail.com>
2022-02-11 05:41:35 -0800

    fix translator

--- QSB/PlayerBodySetup/REM.cs
+++ QSB/PlayerBodySetup/REM.cs
@@ -5,4 +5,5 @@ namespace QSB.PlayerBodySetup
 	public class REM_OWLight2 : OWLight2 { }
 	public class REM_SphereShape : SphereShape { }
 	public class REM_LightSourceVolume : LightSourceVolume { }
+	public class REM_RotateTransform : RotateTransform { }
 }
