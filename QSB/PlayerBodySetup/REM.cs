4157b52789c1e723c90d3d07d7a63e0a698bf52a
JohnCorby <williamforgames6@gmail.com>
2022-02-10 23:16:03 -0800

    proxy scripts for usage in unity project

---
+++ QSB/PlayerBodySetup/REM.cs
@@ -0,0 +1,8 @@
+namespace QSB.PlayerBodySetup
+{
+	public class REM_DitheringAnimator : DitheringAnimator { }
+	public class REM_OWCamera : OWCamera { }
+	public class REM_OWLight2 : OWLight2 { }
+	public class REM_SphereShape : SphereShape { }
+	public class REM_LightSourceVolume : LightSourceVolume { }
+}
