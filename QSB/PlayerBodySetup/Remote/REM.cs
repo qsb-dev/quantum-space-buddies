a756781a2d01ad8031ba29a7a7bd95f6c1a2593a
JohnCorby <williamforgames6@gmail.com>
2022-02-11 20:17:50 -0800

    remove REM.cs, use proxy scripts instead

--- QSB/PlayerBodySetup/Remote/REM.cs
+++
@@ -1,9 +0,0 @@
-namespace QSB.PlayerBodySetup.Remote
-{
-	public class REM_DitheringAnimator : DitheringAnimator { }
-	public class REM_OWCamera : OWCamera { }
-	public class REM_OWLight2 : OWLight2 { }
-	public class REM_SphereShape : SphereShape { }
-	public class REM_LightSourceVolume : LightSourceVolume { }
-	public class REM_RotateTransform : RotateTransform { }
-}
