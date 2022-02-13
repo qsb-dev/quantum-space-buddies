609567a8cc60c3f1fcaaaa8d7d155bd380a5f5dd
JohnCorby <williamforgames6@gmail.com>
2022-02-13 15:51:00 -0800

    Update QSBOrb.cs

--- QSB/OrbSync/WorldObjects/QSBOrb.cs
+++ QSB/OrbSync/WorldObjects/QSBOrb.cs
@@ -51,6 +51,8 @@ namespace QSB.OrbSync.WorldObjects
 				return;
 			}
 
+			DebugLog.DebugWrite($"{this} dragging = {value}");
+
 			if (value)
 			{
 				AttachedObject._isBeingDragged = true;
@@ -65,6 +67,8 @@ namespace QSB.OrbSync.WorldObjects
 				AttachedObject._isBeingDragged = false;
 				AttachedObject._interactibleCollider.enabled = true;
 			}
+
+			DebugLog.DebugWrite($"{this} interactible collider enabled = {AttachedObject._interactibleCollider.enabled}");
 		}
 
 		public void SetSlot(int slotIndex, bool playAudio)
@@ -75,6 +79,8 @@ namespace QSB.OrbSync.WorldObjects
 			{
 				return;
 			}
+
+			DebugLog.DebugWrite($"{this} slot = {slotIndex} {newSlot}");
 
 			if (oldSlot)
 			{
