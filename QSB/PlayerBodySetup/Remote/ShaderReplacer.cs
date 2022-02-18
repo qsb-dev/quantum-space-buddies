027c772b2614447899f0ff496602d2ba91e811c6
JohnCorby <williamforgames6@gmail.com>
2022-02-17 17:37:56 -0800

    update bundle (make active things that used to be enabled in the player/probe/tool creator methods)

--- QSB/PlayerBodySetup/Remote/ShaderReplacer.cs
+++ QSB/PlayerBodySetup/Remote/ShaderReplacer.cs
@@ -8,6 +8,16 @@ namespace QSB.PlayerBodySetup.Remote
 		public static void ReplaceShaders(GameObject prefab)
 		{
 			DebugLog.DebugWrite($"TODO: replace shaders for prefab {prefab}");
+
+			foreach (var renderer in prefab.GetComponentsInChildren<Renderer>())
+			{
+				DebugLog.DebugWrite($"found renderer {renderer}");
+				foreach (var material in renderer.sharedMaterials)
+				{
+					DebugLog.DebugWrite($"found shared material {material}");
+					DebugLog.DebugWrite($"shader = {material.shader}");
+				}
+			}
 		}
 	}
 }
