ea4bc822bd3e71de1d636f5cdd0ba1cddd6867b7
JohnCorby <williamforgames6@gmail.com>
2022-03-10 19:38:59 -0800

    QSBMeteor: format

--- QSB/MeteorSync/WorldObjects/QSBMeteor.cs
+++ QSB/MeteorSync/WorldObjects/QSBMeteor.cs
@@ -22,16 +22,17 @@ public class QSBMeteor : WorldObject<MeteorController>
 	}
 
 	public static bool IsSpecialImpact(GameObject go) =>
-		go == Locator.GetPlayerCollider().gameObject || (Locator.GetProbe() != null && go == Locator.GetProbe()._anchor._collider.gameObject);
+		go == Locator.GetPlayerCollider().gameObject ||
+		Locator.GetProbe() != null && go == Locator.GetProbe()._anchor._collider.gameObject;
 
 	public void SpecialImpact()
 	{
 		AttachedObject._intactRenderer.enabled = false;
 		AttachedObject._impactLight.enabled = true;
 		AttachedObject._impactLight.intensity = AttachedObject._impactLightCurve.Evaluate(0f);
-		foreach (var particleSystem in AttachedObject._impactParticles)
+		foreach (var impactParticle in AttachedObject._impactParticles)
 		{
-			particleSystem.Play();
+			impactParticle.Play();
 		}
 
 		AttachedObject._impactSource.PlayOneShot(AudioType.BH_MeteorImpact);
