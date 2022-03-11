330fda9b2c5268fd61982f86e3f38f65a8727adc
JohnCorby <williamforgames6@gmail.com>
2022-03-11 10:05:37 -0800

    dream rafts: BEGIN!!!

---
+++ QSB/EchoesOfTheEye/DreamRafts/DreamRaftManager.cs
@@ -0,0 +1,13 @@
+﻿using Cysharp.Threading.Tasks;
+using QSB.WorldSync;
+using System.Threading;
+
+namespace QSB.EchoesOfTheEye.DreamRafts;
+
+public class DreamRaftManager : WorldObjectManager
+{
+	public override WorldObjectScene WorldObjectScene => WorldObjectScene.SolarSystem;
+	public override bool DlcOnly => true;
+
+	public override async UniTask BuildWorldObjects(OWScene scene, CancellationToken ct) { }
+}
