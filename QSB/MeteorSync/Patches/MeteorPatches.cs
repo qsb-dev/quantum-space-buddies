22b770ff7fbeeb219295f69bcf31bd933749a1ee
JohnCorby <williamforgames6@gmail.com>
2022-03-11 03:18:47 -0800

    MeteorLauncher_FixedUpdate: check QSBWorldSync.AllObjectsReady

--- QSB/MeteorSync/Patches/MeteorPatches.cs
+++ QSB/MeteorSync/Patches/MeteorPatches.cs
@@ -23,6 +23,11 @@ public class MeteorServerPatches : QSBPatch
 	[HarmonyPatch(typeof(MeteorLauncher), nameof(MeteorLauncher.FixedUpdate))]
 	public static bool MeteorLauncher_FixedUpdate(MeteorLauncher __instance)
 	{
+		if (!QSBWorldSync.AllObjectsReady)
+		{
+			return true;
+		}
+
 		if (__instance._launchedMeteors != null)
 		{
 			for (var i = __instance._launchedMeteors.Count - 1; i >= 0; i--)
