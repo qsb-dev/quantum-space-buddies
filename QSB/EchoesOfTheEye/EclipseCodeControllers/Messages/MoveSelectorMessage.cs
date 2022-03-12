2b32b2e24f76a93c8c269650a789784e5406d17f
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2022-03-12 11:47:44 +0000

    sync code controllers

---
+++ QSB/EchoesOfTheEye/EclipseCodeControllers/Messages/MoveSelectorMessage.cs
@@ -0,0 +1,22 @@
+﻿using QSB.EchoesOfTheEye.EclipseCodeControllers.WorldObjects;
+using QSB.Messaging;
+
+namespace QSB.EchoesOfTheEye.EclipseCodeControllers.Messages;
+
+internal class MoveSelectorMessage : QSBWorldObjectMessage<QSBEclipseCodeController, (int newSelectedDial, bool up)>
+{
+	public MoveSelectorMessage(int selectedDial, bool up) : base((selectedDial, up)) { }
+
+	public override void OnReceiveRemote()
+	{
+		WorldObject.AttachedObject._selectedDial = Data.newSelectedDial;
+		if (WorldObject.AttachedObject.MoveSelectorToLocalPositionY(WorldObject.AttachedObject._dials[Data.newSelectedDial].transform.localPosition.y))
+		{
+			WorldObject.AttachedObject._gearInterfaceVertical.AddRotation(Data.up ? 45f : -45f, 0f);
+		}
+		else
+		{
+			WorldObject.AttachedObject._gearInterfaceVertical.PlayFailure(Data.up, 0.5f);
+		}
+	}
+}
---
+++ QSB/EchoesOfTheEye/EclipseCodeControllers/WorldObjects/QSBEclipseCodeController.cs
@@ -0,0 +1,33 @@
+﻿using Cysharp.Threading.Tasks;
+using QSB.EchoesOfTheEye.EclipseCodeControllers.Messages;
+using QSB.Messaging;
+using QSB.Player;
+using QSB.WorldSync;
+using System.Threading;
+
+namespace QSB.EchoesOfTheEye.EclipseCodeControllers.WorldObjects;
+
+public class QSBEclipseCodeController : WorldObject<EclipseCodeController4>
+{
+	public PlayerInfo PlayerInControl;
+
+	public override void SendInitialState(uint to)
+		=> this.SendMessage(new InitialStateMessage() { To = to });
+
+	public override async UniTask Init(CancellationToken ct)
+	{
+		AttachedObject.gameObject.AddComponent<CodeControllerRemoteUpdater>();
+	}
+
+	public override void OnRemoval()
+	{
+		UnityEngine.Object.Destroy(AttachedObject.gameObject.GetComponent<CodeControllerRemoteUpdater>());
+	}
+
+	public void SetUser(uint user)
+	{
+		var player = QSBPlayerManager.GetPlayer(user);
+		AttachedObject._interactReceiver.SetInteractionEnabled(user == 0 || player == PlayerInControl);
+		PlayerInControl = player;
+	}
+}
