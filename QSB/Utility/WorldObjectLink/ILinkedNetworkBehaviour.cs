dc220611d9a7853bb066cd853180fe9f0aa45feb
JohnCorby <williamforgames6@gmail.com>
2022-03-13 23:31:52 -0700

    api for linking world objects and network behaviours

---
+++ QSB/Utility/WorldObjectLink/ILinkedNetworkBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using QSB.WorldSync;
+
+namespace QSB.Utility.WorldObjectLink;
+
+/// <summary>
+/// a network behaviour that is linked to a world object
+/// </summary>
+public interface ILinkedNetworkBehaviour<out TWorldObject>
+	where TWorldObject : IWorldObject
+{
+	TWorldObject WorldObject { get; }
+	void SetWorldObject(IWorldObject worldObject);
+}
---
+++ QSB/Utility/WorldObjectLink/LinkWorldObjectMessage.cs
@@ -0,0 +1,22 @@
+﻿using Mirror;
+using QSB.Messaging;
+using QSB.WorldSync;
+
+namespace QSB.Utility.WorldObjectLink;
+
+/// <summary>
+/// sent from the host to a non-host
+/// telling a world object and network behaviour to link
+/// </summary>
+public class LinkWorldObjectMessage : QSBWorldObjectMessage<ILinkedWorldObject<NetworkBehaviour>, uint>
+{
+	public LinkWorldObjectMessage(NetworkBehaviour networkBehaviour) :
+		base(networkBehaviour.netId) { }
+
+	public override void OnReceiveRemote()
+	{
+		var networkBehaviour = NetworkClient.spawned[Data].GetComponent<ILinkedNetworkBehaviour<IWorldObject>>();
+		WorldObject.SetNetworkBehaviour((NetworkBehaviour)networkBehaviour);
+		networkBehaviour.SetWorldObject(WorldObject);
+	}
+}
