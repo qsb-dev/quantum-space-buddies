5391bc67963a6b2594b243a8e9740867ecb283d4
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2022-03-14 15:50:56 +0000

    Create AirlockCallToOpenMessage.cs

---
+++ QSB/EchoesOfTheEye/AirlockSync/Messages/AirlockCallToOpenMessage.cs
@@ -0,0 +1,60 @@
+﻿using QSB.EchoesOfTheEye.AirlockSync.WorldObjects;
+using QSB.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QSB.EchoesOfTheEye.AirlockSync.Messages;
+
+internal class AirlockCallToOpenMessage : QSBWorldObjectMessage<QSBGhostAirlock, bool>
+{
+	public AirlockCallToOpenMessage(bool front) : base(front) { }
+
+	public override void OnReceiveRemote()
+	{
+		var airlockInterface = WorldObject.AttachedObject._interface;
+
+		if (Data)
+		{
+			if (airlockInterface._frozenFromFlood)
+			{
+				airlockInterface._backGearEffects.PlayFailure(true, 1f);
+				return;
+			}
+
+			if (airlockInterface._calledToOpenFromOutside)
+			{
+				airlockInterface._frontGearEffects.AddRotation(90f, 1f);
+				return;
+			}
+
+			airlockInterface._frontGearEffects.AddRotation(90f, 1f);
+			airlockInterface._rotatingDirection--;
+			airlockInterface.enabled = true;
+			airlockInterface._calledToOpenFromOutside = true;
+			airlockInterface.CallOnRotateEvent();
+		}
+		else
+		{
+			if (airlockInterface._frozenFromFlood)
+			{
+				airlockInterface._backGearEffects.PlayFailure(true, 1f);
+				return;
+			}
+
+			if (airlockInterface._calledToOpenFromOutside)
+			{
+				airlockInterface._backGearEffects.AddRotation(90f, 1f);
+				return;
+			}
+
+			airlockInterface._backGearEffects.AddRotation(90f, 1f);
+			airlockInterface._rotatingDirection++;
+			airlockInterface.enabled = true;
+			airlockInterface._calledToOpenFromOutside = true;
+			airlockInterface.CallOnRotateEvent();
+		}
+	}
+}
