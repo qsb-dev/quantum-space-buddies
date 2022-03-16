7c5386c688910292d79d48df5cfcd18ecd70ba56
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2022-03-16 13:48:43 +0000

    do elevators

---
+++ QSB/EchoesOfTheEye/EclipseElevators/Patches/EclipseElevatorPatches.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using QSB.EchoesOfTheEye.EclipseElevators.Messages;
+using QSB.EchoesOfTheEye.EclipseElevators.WorldObjects;
+using QSB.Messaging;
+using QSB.Patches;
+using QSB.WorldSync;
+
+namespace QSB.EchoesOfTheEye.EclipseElevators.Patches;
+
+internal class EclipseElevatorPatches : QSBPatch
+{
+	public override QSBPatchTypes Type => QSBPatchTypes.OnClientConnect;
+
+	[HarmonyPrefix]
+	[HarmonyPatch(typeof(ElevatorDestination), nameof(ElevatorDestination.OnPressInteract))]
+	public static bool CallElevator(ElevatorDestination __instance)
+	{
+		__instance.GetWorldObject<QSBElevatorDestination>().SendMessage(new CallElevatorMessage());
+		return true;
+	}
+}
