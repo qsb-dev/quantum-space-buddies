288b4ecd17e4f1acd11ca2db37942d4d1c698f4d
JohnCorby <williamforgames6@gmail.com>
2022-03-25 22:52:53 -0700

    dream raft: catch extra cases of _lit changing (ie RespawnRaft and ExtinguishImmediately)

--- QSB/EchoesOfTheEye/DreamRafts/Patches/DreamRaftPatches.cs
+++ QSB/EchoesOfTheEye/DreamRafts/Patches/DreamRaftPatches.cs
@@ -29,4 +29,30 @@ public class DreamRaftPatches : QSBPatch
 		__instance.GetWorldObject<QSBDreamObjectProjector>()
 			.SendMessage(new SetLitMessage(lit));
 	}
+
+	[HarmonyPrefix]
+	[HarmonyPatch(typeof(DreamRaftProjector), nameof(DreamRaftProjector.RespawnRaft))]
+	private static void RespawnRaft(DreamRaftProjector __instance)
+	{
+		if (Remote)
+		{
+			return;
+		}
+
+		__instance.GetWorldObject<QSBDreamObjectProjector>()
+			.SendMessage(new RespawnRaftMessage());
+	}
+
+	[HarmonyPrefix]
+	[HarmonyPatch(typeof(DreamRaftProjector), nameof(DreamRaftProjector.ExtinguishImmediately))]
+	private static void ExtinguishImmediately(DreamRaftProjector __instance)
+	{
+		if (Remote)
+		{
+			return;
+		}
+
+		__instance.GetWorldObject<QSBDreamObjectProjector>()
+			.SendMessage(new ExtinguishImmediatelyMessage());
+	}
 }
