583756ec5bc7fce49f372302b846fa28b85ffe0f
JohnCorby <williamforgames6@gmail.com>
2022-03-27 00:11:07 -0700

    gksajgklasjgka

---
+++ QSB/EchoesOfTheEye/DreamRafts/Messages/ExtinguishCandlesMessage.cs
@@ -0,0 +1,7 @@
+﻿using QSB.EchoesOfTheEye.DreamRafts.WorldObjects;
+using QSB.Messaging;
+
+namespace QSB.EchoesOfTheEye.DreamRafts.Messages;
+
+public class ExtinguishCandlesMessage : QSBWorldObjectMessage<QSBDreamRaftProjection> { }
+public class EnableRaftMessage : QSBWorldObjectMessage<QSBDreamRaftProjection> { }
