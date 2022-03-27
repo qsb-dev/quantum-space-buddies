aabe9646c253a7e9143db0b22e27e7ad1d71a462
JohnCorby <williamforgames6@gmail.com>
2022-03-27 13:28:49 -0700

    QSBDreamRaftProjector SendInitialState

--- QSB/EchoesOfTheEye/DreamRafts/WorldObjects/QSBDreamRaftProjector.cs
+++ QSB/EchoesOfTheEye/DreamRafts/WorldObjects/QSBDreamRaftProjector.cs
@@ -1,8 +1,16 @@
-﻿using QSB.WorldSync;
+﻿using QSB.EchoesOfTheEye.DreamRafts.Messages;
+using QSB.Messaging;
+using QSB.WorldSync;
 
 namespace QSB.EchoesOfTheEye.DreamRafts.WorldObjects;
 
 public class QSBDreamRaftProjector : WorldObject<DreamRaftProjector>
 {
-	public override void SendInitialState(uint to) { }
+	public override void SendInitialState(uint to)
+	{
+		if (AttachedObject._lit)
+		{
+			this.SendMessage(new SpawnRaftMessage());
+		}
+	}
 }
