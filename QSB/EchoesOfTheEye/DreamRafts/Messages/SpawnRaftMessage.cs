9ac2bedba9e5f2931d7326cd440b6c416395c98e
JohnCorby <williamforgames6@gmail.com>
2022-03-27 13:33:16 -0700

    Update SpawnRaftMessage.cs

--- QSB/EchoesOfTheEye/DreamRafts/Messages/SpawnRaftMessage.cs
+++ QSB/EchoesOfTheEye/DreamRafts/Messages/SpawnRaftMessage.cs
@@ -1,10 +1,11 @@
 ﻿using QSB.EchoesOfTheEye.DreamRafts.WorldObjects;
 using QSB.Messaging;
+using QSB.Patches;
 
 namespace QSB.EchoesOfTheEye.DreamRafts.Messages;
 
 public class SpawnRaftMessage : QSBWorldObjectMessage<QSBDreamRaftProjector>
 {
 	public override void OnReceiveRemote() =>
-		WorldObject.AttachedObject.RespawnRaft();
+		QSBPatch.RemoteCall(WorldObject.AttachedObject.RespawnRaft);
 }
