e6227196e473220788dba4af4bbbddc6d4f307fd
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2022-03-29 18:53:19 +0100

    add projector sync

---
+++ QSB/EchoesOfTheEye/DreamObjectProjectors/Messages/ProjectorStatusMessage.cs
@@ -0,0 +1,12 @@
+﻿using QSB.EchoesOfTheEye.DreamObjectProjectors.WorldObject;
+using QSB.Messaging;
+
+namespace QSB.EchoesOfTheEye.DreamObjectProjectors.Messages;
+
+internal class ProjectorStatusMessage : QSBWorldObjectMessage<QSBDreamObjectProjector, bool>
+{
+	public ProjectorStatusMessage(bool lit) : base(lit) { }
+
+	public override void OnReceiveRemote()
+		=> WorldObject.AttachedObject.SetLit(Data);
+}
---
+++ QSB/EchoesOfTheEye/DreamObjectProjectors/Patches/ProjectorPatches.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using QSB.EchoesOfTheEye.DreamObjectProjectors.Messages;
+using QSB.EchoesOfTheEye.DreamObjectProjectors.WorldObject;
+using QSB.Messaging;
+using QSB.Patches;
+using QSB.WorldSync;
+
+namespace QSB.EchoesOfTheEye.DreamObjectProjectors.Patches;
+
+internal class ProjectorPatches : QSBPatch
+{
+	public override QSBPatchTypes Type => QSBPatchTypes.OnClientConnect;
+
+	[HarmonyPostfix]
+	[HarmonyPatch(typeof(DreamObjectProjector), nameof(DreamObjectProjector.OnPressInteract))]
+	public static void OnPressInteract(DreamObjectProjector __instance)
+		=> __instance.GetWorldObject<QSBDreamObjectProjector>().SendMessage(new ProjectorStatusMessage(false));
+
+	[HarmonyPrefix]
+	[HarmonyPatch(typeof(DreamObjectProjector), nameof(DreamObjectProjector.FixedUpdate))]
+	public static bool FixedUpdate(DreamObjectProjector __instance)
+	{
+		var flag = __instance._lightSensor.IsIlluminated();
+		if (!__instance._lit && flag && !__instance._wasSensorIlluminated)
+		{
+			__instance.SetLit(true);
+			__instance.GetWorldObject<QSBDreamObjectProjector>().SendMessage(new ProjectorStatusMessage(true));
+		}
+
+		__instance._wasSensorIlluminated = flag;
+
+		return false;
+	}
+}
