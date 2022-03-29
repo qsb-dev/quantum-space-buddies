29099bf58fa81f7f8e59d196dbce5a99b8517428
JohnCorby <williamforgames6@gmail.com>
2022-03-29 15:56:39 -0700

    dream candles

---
+++ QSB/EchoesOfTheEye/DreamCandles/Messages/SetLitMessage.cs
@@ -0,0 +1,15 @@
+﻿using QSB.EchoesOfTheEye.DreamCandles.WorldObjects;
+using QSB.Messaging;
+using QSB.Patches;
+
+namespace QSB.EchoesOfTheEye.DreamCandles.Messages;
+
+public class SetLitMessage : QSBWorldObjectMessage<QSBDreamCandle,
+	(bool lit, bool playAudio, bool instant)>
+{
+	public SetLitMessage(bool lit, bool playAudio = true, bool instant = false) :
+		base((lit, playAudio, instant)) { }
+
+	public override void OnReceiveRemote() =>
+		QSBPatch.RemoteCall(() => WorldObject.AttachedObject.SetLit(Data.lit, Data.playAudio, Data.instant));
+}
