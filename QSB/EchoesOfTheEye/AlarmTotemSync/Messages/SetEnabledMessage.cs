13a36bd56c27d8bcb0f444643ba76af7f3004e63
JohnCorby <williamforgames6@gmail.com>
2022-04-01 02:23:59 -0700

    alarm totems: idk

---
+++ QSB/EchoesOfTheEye/AlarmTotemSync/Messages/SetEnabledMessage.cs
@@ -0,0 +1,17 @@
+﻿using QSB.EchoesOfTheEye.AlarmTotemSync.WorldObjects;
+using QSB.Messaging;
+using QSB.Patches;
+
+namespace QSB.EchoesOfTheEye.AlarmTotemSync.Messages;
+
+public class SetEnabledMessage : QSBWorldObjectMessage<QSBAlarmTotem, bool>
+{
+	public SetEnabledMessage(bool data) : base(data) { }
+
+	public override void OnReceiveRemote()
+	{
+		if (!Data && )
+
+		QSBPatch.RemoteCall(() => WorldObject.AttachedObject.SetFaceOpen(Data));
+	}
+}
---
+++ QSB/EchoesOfTheEye/AlarmTotemSync/WorldObjects/QSBAlarmTotem.cs
@@ -0,0 +1,13 @@
+﻿using QSB.EchoesOfTheEye.AlarmTotemSync.Messages;
+using QSB.Messaging;
+using QSB.WorldSync;
+
+namespace QSB.EchoesOfTheEye.AlarmTotemSync.WorldObjects;
+
+public class QSBAlarmTotem : WorldObject<AlarmTotem>
+{
+	public override void SendInitialState(uint to)
+	{
+		this.SendMessage(new SetFaceOpenMessage(AttachedObject._isFaceOpen) { To = to });
+	}
+}
