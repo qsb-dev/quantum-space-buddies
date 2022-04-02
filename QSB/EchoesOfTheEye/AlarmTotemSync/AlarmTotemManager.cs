2afbd878704bff32d02381e8937ba7727e50cab6
JohnCorby <williamforgames6@gmail.com>
2022-04-02 14:15:57 -0700

    QSBAlarmSequenceController

--- QSB/EchoesOfTheEye/AlarmTotemSync/AlarmTotemManager.cs
+++ QSB/EchoesOfTheEye/AlarmTotemSync/AlarmTotemManager.cs
@@ -2,6 +2,7 @@
 using QSB.EchoesOfTheEye.AlarmTotemSync.WorldObjects;
 using QSB.WorldSync;
 using System.Threading;
+using UnityEngine;
 
 namespace QSB.EchoesOfTheEye.AlarmTotemSync;
 
@@ -10,9 +11,18 @@ public class AlarmTotemManager : WorldObjectManager
 	public override WorldObjectScene WorldObjectScene => WorldObjectScene.SolarSystem;
 	public override bool DlcOnly => true;
 
+	private QSBAlarmSequenceController _qsbAlarmSequenceController;
+
 	public override async UniTask BuildWorldObjects(OWScene scene, CancellationToken ct)
 	{
 		QSBWorldSync.Init<QSBAlarmTotem, AlarmTotem>();
 		QSBWorldSync.Init<QSBAlarmBell, AlarmBell>();
+
+		_qsbAlarmSequenceController = new GameObject(nameof(QSBAlarmSequenceController))
+			.AddComponent<QSBAlarmSequenceController>();
+		DontDestroyOnLoad(_qsbAlarmSequenceController.gameObject);
 	}
+
+	public override void UnbuildWorldObjects() =>
+		Destroy(_qsbAlarmSequenceController.gameObject);
 }
