1338251ae12fd24ea5ed1f09f64e0ed2f03c6858
JohnCorby <williamforgames6@gmail.com>
2022-04-05 23:08:30 -0700

    grapple totem: only work for local illumination

---
+++ QSB/EchoesOfTheEye/GrappleTotemSync/Patches/GrappleTotemPatches.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using QSB.EchoesOfTheEye.LightSensorSync.WorldObjects;
+using QSB.Patches;
+using QSB.WorldSync;
+
+namespace QSB.EchoesOfTheEye.GrappleTotemSync.Patches;
+
+public class GrappleTotemPatches : QSBPatch
+{
+	public override QSBPatchTypes Type => QSBPatchTypes.OnClientConnect;
+
+	[HarmonyPrefix]
+	[HarmonyPatch(typeof(LanternZoomPoint), nameof(LanternZoomPoint.OnDetectLight))]
+	private static bool OnDetectLight(LanternZoomPoint __instance) =>
+		!QSBWorldSync.AllObjectsReady ||
+		__instance._lightSensor.GetWorldObject<QSBLightSensor>().IlluminatedByLocal;
+}
---
+++ QSB/EchoesOfTheEye/GrappleTotemSync/WorldObjects/QSBGrappleTotem.cs
@@ -0,0 +1,13 @@
+﻿using QSB.WorldSync;
+
+namespace QSB.EchoesOfTheEye.GrappleTotemSync.WorldObjects;
+
+public class QSBGrappleTotem : WorldObject<LanternZoomPoint>
+{
+	public override void SendInitialState(uint to) { }
+
+	/// <summary>
+	/// todo grapple visuals
+	/// </summary>
+	public void DoGrappleAnimation() { }
+}
