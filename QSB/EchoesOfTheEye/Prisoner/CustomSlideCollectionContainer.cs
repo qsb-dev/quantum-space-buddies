768fe36672399e05e302680f2c0e16124e2a3237
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2022-04-25 21:36:17 +0100

    add cellevator stuff

---
+++ QSB/EchoesOfTheEye/Prisoner/CustomSlideCollectionContainer.cs
@@ -0,0 +1,146 @@
+﻿using QSB.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace QSB.EchoesOfTheEye.Prisoner;
+
+public class CustomSlideCollectionContainer : MonoBehaviour
+{
+	[SerializeField]
+	public CustomSlideCollection _slideCollection = new CustomSlideCollection(0);
+
+	private bool _changeSlidesAllowed;
+	private int _currentSlideIndex;
+	private bool _initialized;
+
+	public int slideCount
+	{
+		get
+		{
+			if (this._slideCollection.slides == null)
+			{
+				return 0;
+			}
+			return this._slideCollection.slides.Length;
+		}
+	}
+
+	public bool isEndOfSlide
+	{
+		get
+		{
+			return this._currentSlideIndex >= this.slideCount - 1;
+		}
+	}
+
+	public int slideIndex
+	{
+		get
+		{
+			return this._currentSlideIndex;
+		}
+		set
+		{
+			if (!this._changeSlidesAllowed)
+			{
+				return;
+			}
+
+			if (this._currentSlideIndex == value)
+			{
+				DebugLog.DebugWrite($"current slide index is already {value}");
+				return;
+			}
+
+			bool forward = this._currentSlideIndex < value;
+			this._currentSlideIndex = value;
+			if (this._currentSlideIndex > this._slideCollection.slides.Length - 1)
+			{
+				this._currentSlideIndex = 0;
+			}
+
+			if (this._currentSlideIndex < 0)
+			{
+				this._currentSlideIndex = this._slideCollection.slides.Length - 1;
+			}
+
+			this.GetCurrentSlide().Display(this, forward);
+		}
+	}
+
+	public OWEvent onSlideTextureUpdated = new OWEvent(1);
+	public OWEvent onEndOfSlides = new OWEvent(1);
+
+	public void Initialize()
+	{
+		DebugLog.DebugWrite($"INITIALIZE");
+		if (this._initialized)
+		{
+			return;
+		}
+		this._changeSlidesAllowed = true;
+		this._initialized = true;
+	}
+
+	public CustomSlide GetCurrentSlide()
+	{
+		if (this._slideCollection.slides.Length == 0)
+		{
+			return null;
+		}
+		return this._slideCollection.slides[this._currentSlideIndex];
+	}
+
+	public Texture GetCurrentSlideTexture()
+	{
+		if (this._slideCollection.slides.Length == 0)
+		{
+			DebugLog.DebugWrite($"NO SLIDES!", OWML.Common.MessageType.Error);
+			return null;
+		}
+		return this.GetCurrentSlide().GetTexture();
+	}
+
+	public void ResetSlideIndex()
+	{
+		this.slideIndex = 0;
+		this.GetCurrentSlide().SetOwner(this);
+	}
+
+	public bool IncreaseSlideIndex()
+	{
+		if (!this._changeSlidesAllowed)
+		{
+			DebugLog.DebugWrite($"Changing slides is not allowed!");
+			return false;
+		}
+
+		int slideIndex = this.slideIndex;
+		this.slideIndex = slideIndex + 1;
+		if (this.slideIndex == 0)
+		{
+			this.onEndOfSlides.Invoke();
+		}
+		return true;
+	}
+
+	public bool DecreaseSlideIndex()
+	{
+		if (!this._changeSlidesAllowed)
+		{
+			return false;
+		}
+		int slideIndex = this.slideIndex;
+		this.slideIndex = slideIndex - 1;
+		return true;
+	}
+
+	public void SetChangeSlidesAllowed(bool allowed)
+	{
+		this._changeSlidesAllowed = allowed;
+	}
+}
---
+++ QSB/EchoesOfTheEye/Prisoner/WorldObjects/QSBPrisonCellElevator.cs
@@ -0,0 +1,95 @@
+﻿using Cysharp.Threading.Tasks;
+using QSB.Audio;
+using QSB.EchoesOfTheEye.Ghosts.WorldObjects;
+using QSB.ItemSync.WorldObjects;
+using QSB.Player;
+using QSB.Utility;
+using QSB.WorldSync;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace QSB.EchoesOfTheEye.Prisoner.WorldObjects;
+
+internal class QSBPrisonCellElevator : WorldObject<PrisonCellElevator>, IQSBDropTarget, IGhostObject
+{
+	public override void SendInitialState(uint to)
+	{
+		// todo : implement this
+	}
+
+	private CustomAutoSlideProjector projector;
+	private OWLight2 light;
+
+	IItemDropTarget IQSBDropTarget.AttachedObject => AttachedObject;
+
+	public override void DisplayLines()
+	{
+		Popcron.Gizmos.Sphere(projector.transform.position, 0.5f, Color.white);
+		Popcron.Gizmos.Line(QSBPlayerManager.LocalPlayer.Body.transform.position, light.transform.position);
+		Popcron.Gizmos.Cone(light.transform.position, light.transform.rotation, light.range, light.GetLight().spotAngle, Color.yellow);
+	}
+
+	public override async UniTask Init(CancellationToken ct)
+	{
+		DebugLog.DebugWrite($"INIT {AttachedObject.name}");
+
+		var Interactibles_PrisonCell = AttachedObject.GetAttachedOWRigidbody().GetOrigParent().parent;
+		DebugLog.DebugWrite(Interactibles_PrisonCell.name);
+
+		var AUTO_SLIDE_PROJECTOR = new GameObject("AUTO SLIDE PROJECTOR");
+		AUTO_SLIDE_PROJECTOR.transform.parent = Interactibles_PrisonCell;
+		AUTO_SLIDE_PROJECTOR.transform.localPosition = new Vector3(-1.8f, 6.4f, 11.33f);
+		AUTO_SLIDE_PROJECTOR.transform.localRotation = Quaternion.identity;
+		AUTO_SLIDE_PROJECTOR.transform.localScale = Vector3.one;
+
+		AUTO_SLIDE_PROJECTOR.SetActive(false);
+
+		var Light = new GameObject("Light");
+		Light.transform.parent = AUTO_SLIDE_PROJECTOR.transform;
+		Light.transform.localPosition = Vector3.zero;
+		Light.transform.localRotation = Quaternion.Euler(32f, 90f, 0f);
+		var lightComponent = Light.AddComponent<Light>();
+		lightComponent.type = LightType.Spot;
+		lightComponent.range = 10;
+		lightComponent.spotAngle = 60;
+		lightComponent.shadows = LightShadows.Soft;
+		lightComponent.shadowStrength = 1f;
+		lightComponent.shadowResolution = UnityEngine.Rendering.LightShadowResolution.Low;
+		lightComponent.shadowBias = 0.05f;
+		lightComponent.shadowNormalBias = 0.4f;
+		lightComponent.shadowNearPlane = 0.2f;
+		light = Light.AddComponent<OWLight2>();
+
+		var projectorComponent = AUTO_SLIDE_PROJECTOR.AddComponent<CustomAutoSlideProjector>();
+		projectorComponent._light = Light.GetComponent<OWLight2>();
+
+		var cellevator1 = QSBCore.Helper.Assets.GetTexture("cellevator1.png");
+		cellevator1.wrapMode = TextureWrapMode.Clamp;
+		var cellevator2 = QSBCore.Helper.Assets.GetTexture("cellevator2.png");
+		cellevator2.wrapMode = TextureWrapMode.Clamp;
+		var cellevator3 = QSBCore.Helper.Assets.GetTexture("cellevator3.png");
+		cellevator3.wrapMode = TextureWrapMode.Clamp;
+
+		var slideCollection = new CustomSlideCollection(3);
+		slideCollection.slides[0] = new CustomSlide() { _image = cellevator1 };
+		slideCollection.slides[1] = new CustomSlide() { _image = cellevator2 };
+		slideCollection.slides[2] = new CustomSlide() { _image = cellevator3 };
+
+		var slideContainer = AUTO_SLIDE_PROJECTOR.AddComponent<CustomSlideCollectionContainer>();
+		slideContainer._slideCollection = slideCollection;
+
+		projectorComponent.SetSlideCollection(slideContainer);
+		projectorComponent._defaultSlideDuration = 1f;
+
+		AUTO_SLIDE_PROJECTOR.SetActive(true);
+
+		projector = projectorComponent;
+
+		projectorComponent.Play(false);
+	}
+}
