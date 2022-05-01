fae10ce7afc52fddae318bbf244ca95be254db5a
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2022-05-01 13:10:42 +0100

    fix prisoner dialogue

---
+++ QSB/Animation/NPC/WorldObjects/PrisonerDialogue.cs
@@ -0,0 +1,12 @@
+﻿namespace QSB.Animation.NPC.WorldObjects;
+
+internal class PrisonerDialogue : NpcAnimController<PrisonerDirector>
+{
+	public override void SendInitialState(uint to)
+	{
+		// todo : implement
+	}
+
+	public override CharacterDialogueTree GetDialogueTree()
+		=> AttachedObject._characterDialogueTree;
+}
