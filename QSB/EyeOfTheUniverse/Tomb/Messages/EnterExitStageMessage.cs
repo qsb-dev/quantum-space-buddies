92ed2ea3cc73018b2f5435c84391a76d778464bc
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2022-05-03 09:44:21 +0100

    add tomb patches + messages

---
+++ QSB/EyeOfTheUniverse/Tomb/Messages/EnterExitStageMessage.cs
@@ -0,0 +1,16 @@
+﻿using QSB.Messaging;
+using QSB.WorldSync;
+using System.Linq;
+
+namespace QSB.EyeOfTheUniverse.Tomb.Messages;
+
+internal class EnterExitStageMessage : QSBMessage<bool>
+{
+	public EnterExitStageMessage(bool enter) : base(enter) { }
+
+	public override void OnReceiveRemote()
+	{
+		var tomb = QSBWorldSync.GetUnityObjects<EyeTombController>().First();
+		tomb._candleController.FadeTo(Data ? 1f : 0f, 1f);
+	}
+}
