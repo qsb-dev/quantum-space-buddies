5bf95ba0366d54f5744a31e7ca8fcb7239fddf17
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2022-05-07 18:42:45 +0100

    only sync position every second

--- QSB/EchoesOfTheEye/RaftSync/TransformSync/RaftTransformSync.cs
+++ QSB/EchoesOfTheEye/RaftSync/TransformSync/RaftTransformSync.cs
@@ -4,12 +4,16 @@ using QSB.Utility;
 using QSB.Utility.LinkedWorldObject;
 using QSB.WorldSync;
 using System;
+using UnityEngine;
 
 namespace QSB.EchoesOfTheEye.RaftSync.TransformSync;
 
 public class RaftTransformSync : UnsectoredRigidbodySync, ILinkedNetworkBehaviour
 {
 	protected override bool UseInterpolation => false;
+
+	private float _lastSetPositionTime;
+	private const float ForcePositionAfterTime = 1;
 
 	private IWorldObject _worldObject;
 	public void SetWorldObject(IWorldObject worldObject) => _worldObject = worldObject;
@@ -71,10 +75,16 @@ public class RaftTransformSync : UnsectoredRigidbodySync, ILinkedNetworkBehaviou
 	protected override void ApplyToAttached()
 	{
 		var targetPos = ReferenceTransform.FromRelPos(transform.position);
-		var targetRot = ReferenceTransform.FromRelRot(transform.rotation);
 
-		AttachedRigidbody.SetPosition(targetPos);
-		AttachedRigidbody.SetRotation(targetRot);
+		if (Time.unscaledTime >= _lastSetPositionTime + ForcePositionAfterTime)
+		{
+			_lastSetPositionTime = Time.unscaledTime;
+
+			var targetRot = ReferenceTransform.FromRelRot(transform.rotation);
+
+			AttachedRigidbody.SetPosition(targetPos);
+			AttachedRigidbody.SetRotation(targetRot);
+		}
 
 		var targetVelocity = ReferenceRigidbody.FromRelVel(Velocity, targetPos);
 		var targetAngularVelocity = ReferenceRigidbody.FromRelAngVel(AngularVelocity);
