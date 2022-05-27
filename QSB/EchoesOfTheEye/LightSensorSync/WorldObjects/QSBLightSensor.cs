b3709b8cdcb28ac317d1f42293c699d4ac944997
JohnCorby <williamforgames6@gmail.com>
2022-05-27 15:03:30 -0700

    Revert "LightSensorAuthorityMessage"
    This reverts commit 7ed6fed50c37967f22243e7d6244320cd8adbad6.

--- QSB/EchoesOfTheEye/LightSensorSync/WorldObjects/QSBLightSensor.cs
+++ QSB/EchoesOfTheEye/LightSensorSync/WorldObjects/QSBLightSensor.cs
@@ -1,19 +1,14 @@
-﻿using QSB.EchoesOfTheEye.LightSensorSync.Messages;
-using QSB.Messaging;
-using QSB.WorldSync;
+﻿using QSB.WorldSync;
 using System;
 
 namespace QSB.EchoesOfTheEye.LightSensorSync.WorldObjects;
 
 internal class QSBLightSensor : WorldObject<SingleLightSensor>
 {
-	public uint AuthorityOwner;
-
 	public bool LocallyIlluminated;
 
 	public Action OnDetectLocalLight;
 	public Action OnDetectLocalDarkness;
 
-	public override void SendInitialState(uint to) =>
-		this.SendMessage(new LightSensorAuthorityMessage(AuthorityOwner) { To = to });
+	public override void SendInitialState(uint to) { }
 }
