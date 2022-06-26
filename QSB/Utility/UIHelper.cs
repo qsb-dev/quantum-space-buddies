4ca096ba7138923509516cafa5f34a4ee1ed8c53
JohnCorby <williamforgames6@gmail.com>
2022-06-26 11:38:45 -0700

    move CultureInfo to QSBLocalization

--- QSB/Utility/UIHelper.cs
+++ QSB/Utility/UIHelper.cs
@@ -1,6 +1,4 @@
-﻿using QSB.Localisation;
-using System.Globalization;
-using System.Linq;
+﻿using System.Linq;
 
 namespace QSB.Utility;
 
@@ -21,28 +19,4 @@ internal static class UIHelper
 		DebugLog.DebugWrite($"Added text \"{text}\" to the UI table with index {key}");
 		return (UITextType)key;
 	}
-
-	public static CultureInfo GetCurrentCultureInfo() 
-		=> QSBLocalization.Current.Language switch
-		{
-			/*
-			 * Language tags from BCP-47 standard, implemented by windows
-			 * https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-lcid/a9eac961-e77d-41a6-90a5-ce1a8b0cdb9c
-			 * I have no fucking idea if this will work on linux. ¯\_(ツ)_/¯
-			 */
-
-			TextTranslation.Language.ENGLISH => new CultureInfo("en"),
-			TextTranslation.Language.SPANISH_LA => new CultureInfo("es-419"),
-			TextTranslation.Language.GERMAN => new CultureInfo("de"),
-			TextTranslation.Language.FRENCH => new CultureInfo("fr"),
-			TextTranslation.Language.ITALIAN => new CultureInfo("it"),
-			TextTranslation.Language.POLISH => new CultureInfo("pl"),
-			TextTranslation.Language.PORTUGUESE_BR => new CultureInfo("pt-BR"),
-			TextTranslation.Language.JAPANESE => new CultureInfo("ja"),
-			TextTranslation.Language.RUSSIAN => new CultureInfo("ru"),
-			TextTranslation.Language.CHINESE_SIMPLE => new CultureInfo("zh-Hans"),
-			TextTranslation.Language.KOREAN => new CultureInfo("ko"),
-			TextTranslation.Language.TURKISH => new CultureInfo("tr"),
-			_ => new CultureInfo("en") // what
-		};
 }
