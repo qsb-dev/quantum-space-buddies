fde6954af296829a1cc0b910e7cd691cd66b1cf8
JohnCorby <williamforgames6@gmail.com>
2022-07-22 10:48:49 -0700

    use MaxBy

--- QSB/SaveSync/QSBProfileManager.cs
+++ QSB/SaveSync/QSBProfileManager.cs
@@ -1,15 +1,12 @@
-﻿using System;
+﻿using QSB.Utility;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace QSB.SaveSync;
 
 public static class QSBProfileManager
 {
-	public static List<QSBProfileData> _profiles = new();
+	public static readonly List<QSBProfileData> _profiles = new();
 	public static QSBProfileData _currentProfile;
 	public static QSBProfileData mostRecentProfile
-		=> Enumerable.FirstOrDefault(Enumerable.OrderByDescending(_profiles, (QSBProfileData profile) => profile.lastModifiedTime));
+		=> _profiles.MaxBy(x => x.lastModifiedTime);
 }
