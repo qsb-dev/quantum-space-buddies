6b786b58286f4b679c128c83150f71a4136cb0a6
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2022-07-23 09:45:30 +0100

    Update DebugTriggerSupernovaMessage.cs

--- QSB/Utility/Messages/DebugTriggerSupernovaMessage.cs
+++ QSB/Utility/Messages/DebugTriggerSupernovaMessage.cs
@@ -5,5 +5,11 @@ namespace QSB.Utility.Messages;
 public class DebugTriggerSupernovaMessage : QSBMessage
 {
 	public override void OnReceiveLocal() => OnReceiveRemote();
-	public override void OnReceiveRemote() => TimeLoop.SetSecondsRemaining(0);
+	public override void OnReceiveRemote()
+	{
+		PlayerData.SaveLoopCount(2);
+		TimeLoop.SetTimeLoopEnabled(true);
+		TimeLoop._isTimeFlowing = true;
+		TimeLoop.SetSecondsRemaining(0);
+	}
 }
