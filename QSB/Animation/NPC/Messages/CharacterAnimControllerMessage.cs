689e2719d0b2d632ffbe2312d33edc9f0a44bb99
ShoosGun <ivanrwpf@gmail.com>
2022-08-11 11:35:58 -0300

    Added QSBCharacterAnimController SendInitialState

---
+++ QSB/Animation/NPC/Messages/CharacterAnimControllerMessage.cs
@@ -0,0 +1,11 @@
+﻿using QSB.Animation.NPC.WorldObjects;
+using QSB.Messaging;
+
+namespace QSB.Animation.NPC.Messages;
+
+internal class CharacterAnimControllerMessage : QSBWorldObjectMessage<QSBCharacterAnimController, bool>
+{
+	public CharacterAnimControllerMessage(bool isInConversation) : base(isInConversation) { }
+
+	public override void OnReceiveRemote() => WorldObject.SetInConversation(Data);
+}
--- QSB/Animation/NPC/WorldObjects/QSBCharacterAnimController.cs
+++ QSB/Animation/NPC/WorldObjects/QSBCharacterAnimController.cs
@@ -1,17 +1,27 @@
-﻿using QSB.WorldSync;
+﻿using QSB.Animation.NPC.Messages;
+using QSB.Messaging;
+using QSB.WorldSync;
 
 namespace QSB.Animation.NPC.WorldObjects;
 
 internal class QSBCharacterAnimController : WorldObject<CharacterAnimController>
 {
-	public override void SendInitialState(uint to)
-	{
-		// todo SendInitialState
-	}
+    public override void SendInitialState(uint to)
+        => this.SendMessage(new CharacterAnimControllerMessage(InConversation()) { To = to });
 
-	public CharacterDialogueTree GetDialogueTree()
-		=> AttachedObject._dialogueTree;
+    public CharacterDialogueTree GetDialogueTree()
+        => AttachedObject._dialogueTree;
 
-	public bool InConversation()
-		=> AttachedObject._inConversation;
+    public void SetInConversation(bool inConversation)
+    {
+        AttachedObject._inConversation = inConversation;
+        if (AttachedObject._hasTalkAnimation && AttachedObject._animator != null)
+        {
+            string animationState = inConversation ? "Talking" : "Idle";
+            AttachedObject._animator.SetTrigger(animationState);
+        }
+    }
+
+    public bool InConversation()
+        => AttachedObject._inConversation;
 }
