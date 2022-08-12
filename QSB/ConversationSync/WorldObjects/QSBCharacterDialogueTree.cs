7d88df8cb9c16a558c1b7f28cb58b2fb5cd41920
ShoosGun <ivanrwpf@gmail.com>
2022-08-12 14:20:51 -0300

    Fixed and reworked the SendInitialState of QSBCharacterDialogueTree

--- QSB/ConversationSync/WorldObjects/QSBCharacterDialogueTree.cs
+++ QSB/ConversationSync/WorldObjects/QSBCharacterDialogueTree.cs
@@ -1,11 +1,7 @@
 ﻿using QSB.ConversationSync.Messages;
 using QSB.Messaging;
+using QSB.Player;
 using QSB.WorldSync;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace QSB.ConversationSync.WorldObjects;
 
@@ -13,12 +9,13 @@ public class QSBCharacterDialogueTree : WorldObject<CharacterDialogueTree>
 {
 	public override void SendInitialState(uint to)
 	{
-		if (AttachedObject.enabled && AttachedObject._currentNode != null)//Only sends if there is a conversation happening
+		uint playeID = ConversationManager.Instance.GetPlayerTalkingToTree(AttachedObject);
+		if (QSBPlayerManager.LocalPlayerId == playeID)//Only sends if the local player is the one talking to the tree
 		{
 			var key = AttachedObject._currentNode._name + AttachedObject._currentNode._listPagesToDisplay[AttachedObject._currentNode._currentPage]; this.SendMessage(new ConversationStartEndMessage(true) { To = to });//To tell the conversation has started
-			new ConversationMessage(ConversationType.Character, ObjectId, key) { To = to }.Send();//To tell that there is a conversation bubble
 			this.SendMessage(new ConversationStartEndMessage(true) { To = to });//To tell the conversation has started
 			new ConversationMessage(ConversationType.Character, ObjectId, key) { To = to }.Send();//To tell that there is a conversation bubble
+			//new ConversationMessage(ConversationType.Player, ObjectId, key) { To = to }.Send();//To tell that there is a conversation bubble with option 
 		}
 	}
 }
