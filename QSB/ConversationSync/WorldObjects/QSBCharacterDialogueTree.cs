716f6503e9187784b51103ddf1c89dfe89573b62
ShoosGun <ivanrwpf@gmail.com>
2022-08-12 14:10:58 -0300

    Added SendInitialState to QSBCharacterDialogueTree

--- QSB/ConversationSync/WorldObjects/QSBCharacterDialogueTree.cs
+++ QSB/ConversationSync/WorldObjects/QSBCharacterDialogueTree.cs
@@ -1,4 +1,6 @@
-﻿using QSB.WorldSync;
+﻿using QSB.ConversationSync.Messages;
+using QSB.Messaging;
+using QSB.WorldSync;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +13,12 @@ public class QSBCharacterDialogueTree : WorldObject<CharacterDialogueTree>
 {
 	public override void SendInitialState(uint to)
 	{
-		// todo : implement this??? does it need it?
+		if (AttachedObject.enabled && AttachedObject._currentNode != null)//Only sends if there is a conversation happening
+		{
+			var key = AttachedObject._currentNode._name + AttachedObject._currentNode._listPagesToDisplay[AttachedObject._currentNode._currentPage]; this.SendMessage(new ConversationStartEndMessage(true) { To = to });//To tell the conversation has started
+			new ConversationMessage(ConversationType.Character, ObjectId, key) { To = to }.Send();//To tell that there is a conversation bubble
+			this.SendMessage(new ConversationStartEndMessage(true) { To = to });//To tell the conversation has started
+			new ConversationMessage(ConversationType.Character, ObjectId, key) { To = to }.Send();//To tell that there is a conversation bubble
+		}
 	}
 }
