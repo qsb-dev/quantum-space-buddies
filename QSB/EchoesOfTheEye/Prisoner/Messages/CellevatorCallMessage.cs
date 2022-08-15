41013b62ede901929eb220d3c8c9a6cb2bcd5cd7
JohnCorby <williamforgames6@gmail.com>
2022-08-15 14:24:48 -0700

    fix cellevator

--- QSB/EchoesOfTheEye/Prisoner/Messages/CellevatorCallMessage.cs
+++ QSB/EchoesOfTheEye/Prisoner/Messages/CellevatorCallMessage.cs
@@ -1,13 +1,10 @@
 ﻿using QSB.EchoesOfTheEye.Prisoner.WorldObjects;
 using QSB.Messaging;
-using QSB.Patches;
 
 namespace QSB.EchoesOfTheEye.Prisoner.Messages;
 
 internal class CellevatorCallMessage : QSBWorldObjectMessage<QSBPrisonCellElevator, int>
 {
 	public CellevatorCallMessage(int floorIndex) : base(floorIndex) { }
-
-	public override void OnReceiveRemote() =>
-		QSBPatch.RemoteCall(() => WorldObject.AttachedObject.CallElevatorToFloor(Data));
+	public override void OnReceiveRemote() => WorldObject.AttachedObject.CallElevatorToFloor(Data);
 }
