acb91e31e48b6032a5776735e59d951f351688fc
JohnCorby <williamforgames6@gmail.com>
2022-08-18 22:28:21 -0700

    initial state

--- QSB/WarpPadSync/WorldObjects/QSBWarpPad.cs
+++ QSB/WarpPadSync/WorldObjects/QSBWarpPad.cs
@@ -1,4 +1,6 @@
-﻿using QSB.WorldSync;
+﻿using QSB.Messaging;
+using QSB.WarpPadSync.Messages;
+using QSB.WorldSync;
 
 namespace QSB.WarpPadSync.WorldObjects;
 
@@ -6,6 +8,12 @@ public class QSBWarpPad : WorldObject<NomaiWarpPlatform>
 {
 	public override void SendInitialState(uint to)
 	{
-		//todo
+		if (AttachedObject.IsBlackHoleOpen())
+		{
+			this.SendMessage(new OpenBlackHoleMessage(
+				AttachedObject._linkedPlatform.GetWorldObject<QSBWarpPad>().ObjectId,
+				AttachedObject._keepBlackHoleOpen
+			));
+		}
 	}
 }
