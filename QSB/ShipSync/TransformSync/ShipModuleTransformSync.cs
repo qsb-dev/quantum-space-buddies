6cc191c8d6341b0a5cb035e437a39c0f7a1273ad
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2022-08-24 08:15:42 +0100

    null checks for ship leg/module syncs

--- QSB/ShipSync/TransformSync/ShipModuleTransformSync.cs
+++ QSB/ShipSync/TransformSync/ShipModuleTransformSync.cs
@@ -15,7 +15,11 @@ internal class ShipModuleTransformSync : SectoredRigidbodySync, ILinkedNetworkBe
 		=> AttachedTransform
 			&& base.CheckValid();
 
-	protected override bool CheckReady() => base.CheckReady() && _qsbModule.AttachedObject.isDetached;
+	protected override bool CheckReady()
+		=> base.CheckReady()
+		&& _qsbModule != null
+		&& _qsbModule.AttachedObject != null
+		&& _qsbModule.AttachedObject.isDetached;
 
 	protected override bool UseInterpolation => true;
 
