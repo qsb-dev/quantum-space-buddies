5ceca388c3c931b8dfb90bc77913be192e0c62f5
Nick <nicholasconnors@hotmail.ca>
2022-08-26 18:33:42 -0400

    Rename from TransformSync to VariableSync

---
+++ QSB/StationaryProbeLauncherSync/VariableSync/StationaryProbeLauncherVariableSync.cs
@@ -0,0 +1,33 @@
+﻿using Mirror;
+using QSB.EchoesOfTheEye;
+using QSB.StationaryProbeLauncherSync.WorldObjects;
+using UnityEngine;
+
+namespace QSB.StationaryProbeLauncherSync.VariableSync;
+
+public class StationaryProbeLauncherVariableSync : RotatingElementsVariableSyncer<QSBStationaryProbeLauncher>
+{
+    protected override Transform[] RotatingElements => new Transform[] { WorldObject.AttachedObject.transform };
+
+    protected override void Serialize(NetworkWriter writer)
+    {
+        base.Serialize(writer);
+
+        var launcher = WorldObject.AttachedObject as StationaryProbeLauncher;
+
+        writer.Write(launcher._degreesX);
+        writer.Write(launcher._degreesY);
+        writer.Write(launcher._audioSource.GetLocalVolume());
+    }
+
+    protected override void Deserialize(NetworkReader reader)
+    {
+        base.Deserialize(reader);
+
+        var launcher = WorldObject.AttachedObject as StationaryProbeLauncher;
+
+        launcher._degreesX = reader.Read<float>();
+        launcher._degreesY = reader.Read<float>();
+        launcher._audioSource.SetLocalVolume(reader.Read<float>());
+    }
+}
