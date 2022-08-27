423c3803caba115e6c10bd384d097593fcaf9842
JohnCorby <williamforgames6@gmail.com>
2022-08-27 11:21:09 -0700

    im a dumbass

--- QSB/Tools/ProbeLauncherTool/WorldObjects/QSBStationaryProbeLauncher.cs
+++ QSB/Tools/ProbeLauncherTool/WorldObjects/QSBStationaryProbeLauncher.cs
@@ -10,12 +10,12 @@ using System.Threading;
 
 namespace QSB.Tools.ProbeLauncherTool.WorldObjects;
 
-public class QSBStationaryProbeLauncher : QSBProbeLauncher, ILinkedWorldObject<StationaryProbeLauncherVariableSync>
+public class QSBStationaryProbeLauncher : QSBProbeLauncher, ILinkedWorldObject<StationaryProbeLauncherVariableSyncer>
 {
 	private uint _currentUser = uint.MaxValue;
 
-	public StationaryProbeLauncherVariableSync NetworkBehaviour { get; private set; }
-	public void SetNetworkBehaviour(NetworkBehaviour networkBehaviour) => NetworkBehaviour = (StationaryProbeLauncherVariableSync)networkBehaviour;
+	public StationaryProbeLauncherVariableSyncer NetworkBehaviour { get; private set; }
+	public void SetNetworkBehaviour(NetworkBehaviour networkBehaviour) => NetworkBehaviour = (StationaryProbeLauncherVariableSyncer)networkBehaviour;
 
 	private bool _isInUse;
 	private StationaryProbeLauncher _stationaryProbeLauncher;
