d3dc4f4023ef68f4c1aa0c7b684e365d33e922b2
Nick <nicholasconnors@hotmail.ca>
2022-08-27 19:10:20 -0400

    Create QSBAudioSourceOneShotTracker + use it to sync impact sounds

--- QSB/Audio/Messages/PlayerAudioControllerOneShotMessage.cs
+++ QSB/Audio/Messages/PlayerAudioControllerOneShotMessage.cs
@@ -5,12 +5,12 @@ using QSB.WorldSync;
 namespace QSB.Audio.Messages;
 
 
-public class PlayerAudioControllerOneShotMessage : QSBMessage<(AudioType audioType, uint userID)>
+public class PlayerAudioControllerOneShotMessage : QSBMessage<(AudioType audioType, uint userID, float pitch, float volume)>
 {
-	public PlayerAudioControllerOneShotMessage(AudioType audioType, uint userID) : base((audioType, userID)) { }
+	public PlayerAudioControllerOneShotMessage(AudioType audioType, uint userID, float pitch = 1f, float volume = 1f) : base((audioType, userID, pitch, volume)) { }
 
 	public override bool ShouldReceive => QSBWorldSync.AllObjectsReady;
 
 	public override void OnReceiveRemote() =>
-		QSBPlayerManager.GetPlayer(Data.userID)?.AudioController?.PlayOneShot(Data.audioType);
+		QSBPlayerManager.GetPlayer(Data.userID)?.AudioController?.PlayOneShot(Data.audioType, Data.pitch, Data.volume);
 }
---
+++ QSB/Audio/Patches/PlayerImpactAudioPatches.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using QSB.Audio.Messages;
+using QSB.Messaging;
+using QSB.Patches;
+using QSB.Player;
+
+namespace QSB.Audio.Patches;
+
+internal class PlayerImpactAudioPatches : QSBPatch
+{
+	public override QSBPatchTypes Type => QSBPatchTypes.OnClientConnect;
+
+	[HarmonyPostfix]
+	[HarmonyPatch(typeof(PlayerImpactAudio), nameof(PlayerImpactAudio.Start))]
+	public static void PlayerImpactAudio_Start(PlayerImpactAudio __instance)
+	{
+		__instance.gameObject.AddComponent<QSBAudioSourceOneShotTracker>();
+	}
+
+	[HarmonyPrefix]
+	[HarmonyPatch(typeof(PlayerImpactAudio), nameof(PlayerImpactAudio.OnImpact))]
+	public static void PlayerImpactAudio_OnImpact_Prefix(PlayerImpactAudio __instance) =>
+		// First we reset in case no audio is actually played
+		__instance.gameObject.GetComponent<QSBAudioSourceOneShotTracker>()?.Reset();
+
+	[HarmonyPostfix]
+	[HarmonyPatch(typeof(PlayerImpactAudio), nameof(PlayerImpactAudio.OnImpact))]
+	public static void PlayerImpactAudio_OnImpact_Postfix(PlayerImpactAudio __instance)
+	{
+		var tracker = __instance.gameObject.GetComponent<QSBAudioSourceOneShotTracker>();
+		if (tracker)
+		{
+			if (tracker.LastPlayed != AudioType.None)
+			{
+				new PlayerAudioControllerOneShotMessage(tracker.LastPlayed, QSBPlayerManager.LocalPlayerId, tracker.Pitch, tracker.Volume).Send();
+			}
+		}
+	}
+}
