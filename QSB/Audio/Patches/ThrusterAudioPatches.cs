882270a6f96527629277e853bda027780f00e573
Nick <nicholasconnors@hotmail.ca>
2022-08-28 00:05:00 -0400

    Sync rotational thruster sounds

---
+++ QSB/Audio/Patches/ThrusterAudioPatches.cs
@@ -0,0 +1,45 @@
+﻿using HarmonyLib;
+using QSB.Audio.Messages;
+using QSB.Messaging;
+using QSB.Patches;
+using QSB.Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QSB.Audio.Patches;
+
+internal class ThrusterAudioPatches : QSBPatch
+{
+	// Since we patch Start we do it when the mod starts, else it won't run
+	public override QSBPatchTypes Type => QSBPatchTypes.OnModStart;
+
+	[HarmonyPostfix]
+	[HarmonyPatch(typeof(ThrusterAudio), nameof(ThrusterAudio.Start))]
+	public static void ThrusterAudio_Start(ThrusterAudio __instance)
+	{
+		__instance._rotationalSource.gameObject.AddComponent<QSBAudioSourceOneShotTracker>();
+	}
+
+	[HarmonyPrefix]
+	[HarmonyPatch(typeof(ThrusterAudio), nameof(ThrusterAudio.OnFireRotationalThruster))]
+	public static void ThrusterAudio_OnFireRotationalThruster_Prefix(ThrusterAudio __instance) =>
+		// First we reset in case no audio is actually played
+		__instance._rotationalSource.gameObject.GetComponent<QSBAudioSourceOneShotTracker>()?.Reset();
+
+	[HarmonyPostfix]
+	[HarmonyPatch(typeof(ThrusterAudio), nameof(ThrusterAudio.OnFireRotationalThruster))]
+	public static void ThrusterAudio_OnFireRotationalThruster_Postfix(ThrusterAudio __instance)
+	{
+		if (__instance._rotationalSource.gameObject.TryGetComponent<QSBAudioSourceOneShotTracker>(out var tracker))
+		{
+			if (tracker.LastPlayed != AudioType.None)
+			{
+				if (__instance is ShipThrusterAudio)
+					new ShipThrusterAudioOneShotMessage(tracker.LastPlayed, tracker.Pitch, tracker.Volume).Send();
+			}
+		}
+	}
+}
