2f680398aa3b0e36b1cf062d66f8ba200bec6df7
JohnCorby <williamforgames6@gmail.com>
2022-09-10 17:02:33 -0700

    remove #if thing

--- QSB/QSBCore.cs
+++ QSB/QSBCore.cs
@@ -245,7 +245,6 @@ public class QSBCore : ModBehaviour
 		IncompatibleModsAllowed = config.GetSettingsValue<bool>("incompatibleModsAllowed");
 	}
 
-#if DEBUG
 	private void Update()
 	{
 		if (Keyboard.current[Key.Q].isPressed && Keyboard.current[Key.D].wasPressedThisFrame)
@@ -260,7 +259,6 @@ public class QSBCore : ModBehaviour
 			DebugLog.ToConsole($"DEBUG MODE = {DebugSettings.DebugMode}");
 		}
 	}
-#endif
 }
 
 /*
