3f3eceee6032cfcb89bccd65fbcc8b552273b320
JohnCorby <williamforgames6@gmail.com>
2022-09-28 17:46:02 -0700

    QSBWineCellarSwitch: initial state

--- QSB/EchoesOfTheEye/WineCellar/WorldObjects/QSBWineCellarSwitch.cs
+++ QSB/EchoesOfTheEye/WineCellar/WorldObjects/QSBWineCellarSwitch.cs
@@ -1,5 +1,16 @@
-﻿using QSB.WorldSync;
+﻿using QSB.EchoesOfTheEye.WineCellar.Messages;
+using QSB.Messaging;
+using QSB.WorldSync;
 
 namespace QSB.EchoesOfTheEye.WineCellar.WorldObjects;
 
-internal class QSBWineCellarSwitch : WorldObject<WineCellarSwitch> { }
+internal class QSBWineCellarSwitch : WorldObject<WineCellarSwitch>
+{
+	public override void SendInitialState(uint to)
+	{
+		if (AttachedObject.enabled)
+		{
+			this.SendMessage(new WineCellarSwitchMessage { To = to });
+		}
+	}
+}
