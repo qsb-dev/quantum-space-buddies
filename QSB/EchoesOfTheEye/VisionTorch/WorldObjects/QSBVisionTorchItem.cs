d9efdb3e51a11d7df26ce7827f19c8a43f4e9ba1
JohnCorby <williamforgames6@gmail.com>
2022-10-10 22:26:12 -0700

    move vision torch to own folder

---
+++ QSB/EchoesOfTheEye/VisionTorch/WorldObjects/QSBVisionTorchItem.cs
@@ -0,0 +1,8 @@
+﻿using QSB.ItemSync.WorldObjects.Items;
+
+namespace QSB.EchoesOfTheEye.VisionTorch.WorldObjects;
+
+/// <summary>
+/// TODO: SYNC THIS SHIT LMAOOOOOO
+/// </summary>
+internal class QSBVisionTorchItem : QSBItem<VisionTorchItem> { }
