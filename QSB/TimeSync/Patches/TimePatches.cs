165ae8fdaffaa784bcc73ecba82229ef499ba7e0
JohnCorby <williamforgames6@gmail.com>
2022-11-06 16:40:31 -0800

    just prevent pausing instead of all input

--- QSB/TimeSync/Patches/TimePatches.cs
+++ QSB/TimeSync/Patches/TimePatches.cs
@@ -25,8 +25,8 @@ internal class TimePatches : QSBPatch
 	public static void PlayerCameraEffectController_WakeUp(PlayerCameraEffectController __instance)
 	{
 		// prevent funny thing when you pause while waking up
-		QSBInputManager.Instance.SetInputsEnabled(false);
-		Delay.RunWhen(() => !__instance._isOpeningEyes, () => QSBInputManager.Instance.SetInputsEnabled(true));
+		Locator.GetPauseCommandListener().AddPauseCommandLock();
+		Delay.RunWhen(() => !__instance._isOpeningEyes, () => Locator.GetPauseCommandListener().RemovePauseCommandLock());
 	}
 
 	[HarmonyPrefix]
