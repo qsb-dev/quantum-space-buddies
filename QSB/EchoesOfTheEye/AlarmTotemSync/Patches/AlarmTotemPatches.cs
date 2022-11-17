a07f8a2e68d895134502da03821c3b5a6acfeab0
JohnCorby <williamforgames6@gmail.com>
2022-11-17 01:55:40 -0800

    what am i doing

--- QSB/EchoesOfTheEye/AlarmTotemSync/Patches/AlarmTotemPatches.cs
+++ QSB/EchoesOfTheEye/AlarmTotemSync/Patches/AlarmTotemPatches.cs
@@ -70,6 +70,33 @@ public class AlarmTotemPatches : QSBPatch
 	}
 
 	[HarmonyPrefix]
+	[HarmonyPatch(typeof(AlarmTotem), nameof(AlarmTotem.FixedUpdate))]
+	private static bool FixedUpdate(AlarmTotem __instance)
+	{
+		var isPlayerVisible = __instance._isPlayerVisible;
+		__instance._isPlayerVisible = __instance.CheckPlayerVisible();
+		if (!isPlayerVisible && __instance._isPlayerVisible)
+		{
+			Locator.GetAlarmSequenceController().IncreaseAlarmCounter();
+			__instance._secondsConcealed = 0f;
+			__instance._simTotemMaterials[0] = __instance._simAlarmMaterial;
+			__instance._simTotemRenderer.sharedMaterials = __instance._simTotemMaterials;
+			__instance._simVisionConeRenderer.SetColor(__instance._simAlarmColor);
+			GlobalMessenger.FireEvent("AlarmTotemTriggered");
+		}
+		else if (isPlayerVisible && !__instance._isPlayerVisible)
+		{
+			Locator.GetAlarmSequenceController().DecreaseAlarmCounter();
+			__instance._secondsConcealed = 0f;
+			__instance._simTotemMaterials[0] = __instance._origSimEyeMaterial;
+			__instance._simTotemRenderer.sharedMaterials = __instance._simTotemMaterials;
+			__instance._simVisionConeRenderer.SetColor(__instance._simVisionConeRenderer.GetOriginalColor());
+			__instance._pulseLightController.FadeTo(0f, 0.5f);
+		}
+		return false;
+	}
+
+	[HarmonyPrefix]
 	[HarmonyPatch(typeof(AlarmTotem), nameof(AlarmTotem.CheckPlayerVisible))]
 	private static bool CheckPlayerVisible(AlarmTotem __instance, out bool __result)
 	{
