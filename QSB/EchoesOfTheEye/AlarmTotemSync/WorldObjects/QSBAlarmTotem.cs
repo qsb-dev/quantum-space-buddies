23dd98bd7df67c2a1ee0b291ea018c55027c2b02
JohnCorby <williamforgames6@gmail.com>
2022-11-17 18:15:18 -0800

    goob

--- QSB/EchoesOfTheEye/AlarmTotemSync/WorldObjects/QSBAlarmTotem.cs
+++ QSB/EchoesOfTheEye/AlarmTotemSync/WorldObjects/QSBAlarmTotem.cs
@@ -4,9 +4,6 @@ using QSB.Messaging;
 
 namespace QSB.EchoesOfTheEye.AlarmTotemSync.WorldObjects;
 
-/// <summary>
-/// TODO: make this not NRE (by not doing enable sync) and then readd it back in
-/// </summary>
 public class QSBAlarmTotem : AuthWorldObject<AlarmTotem>
 {
 	public override bool CanOwn => AttachedObject.enabled;
