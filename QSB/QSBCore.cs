08270f9e88bcf10508357405602e71020f3c3fb5
_nebula <41904486+misternebula@users.noreply.github.com>
2023-03-06 21:17:31 +0000

    aaaa

--- QSB/QSBCore.cs
+++ QSB/QSBCore.cs
@@ -152,18 +152,22 @@ public class QSBCore : ModBehaviour
 		else
 		{
 			DebugLog.DebugWrite($"Overriding AppID...");
-			SteamManager.s_EverInitialized = false;
-			SteamManager.s_instance.m_bInitialized = false;
-			var temp = SteamManager.s_instance;
-			SteamManager.s_instance = null;
+			//SteamManager.s_EverInitialized = false;
+			typeof(SteamManager).GetField("s_EverInitialized", BindingFlags.NonPublic | BindingFlags.Static).SetValue(null, false);
+			//SteamManager.s_instance.m_bInitialized = false;
+			var steamManagerInstance = typeof(SteamManager).GetField("s_instance", BindingFlags.NonPublic | BindingFlags.Static).GetValue(null);
+			typeof(SteamManager).GetField("m_bInitialized", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(steamManagerInstance, false);
+			//var temp = SteamManager.s_instance;
+			//var temp = steamManagerInstance;
+			//SteamManager.s_instance = null;
+			typeof(SteamManager).GetField("s_instance", BindingFlags.NonPublic | BindingFlags.Static).SetValue(null, null);
 			SteamAPI.Shutdown();
 
 			System.Environment.SetEnvironmentVariable("SteamAppId", "480");
 			System.Environment.SetEnvironmentVariable("SteamGameId", "480");
 
-			temp.InitializeOnAwake();
-
-
+			//temp.InitializeOnAwake();
+			typeof(SteamManager).GetMethod("InitializeOnAwake", BindingFlags.Public | BindingFlags.Instance).Invoke(steamManagerInstance, null);
 		}
 	}
 
