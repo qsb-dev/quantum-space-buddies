c78f40a8ac5395970a45b00e00bbb62e8a793471
_nebula <41904486+misternebula@users.noreply.github.com>
2023-03-06 16:04:08 +0000

    add prepatcher

---
+++ QSBPrepatch/PatchSteamFiles.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace QSBPrepatch;
+
+internal class PatchSteamFiles
+{
+	static void Main(string[] args)
+	{
+		var basePath = args.Length > 0 ? args[0] : ".";
+		var gamePath = AppDomain.CurrentDomain.BaseDirectory;
+		var dataPath = GetDataPath(gamePath);
+		var managedPath = Path.Combine(dataPath, @"Managed");
+
+		var libDir = new DirectoryInfo(Path.Combine(basePath, "lib"));
+		var files = libDir.GetFiles();
+		foreach (var file in files)
+		{
+			var tempPath = Path.Combine(managedPath, file.Name);
+			file.CopyTo(tempPath, true);
+		}
+	}
+
+	private static string GetExecutableName(string gamePath)
+	{
+		var executableNames = new[] { "Outer Wilds.exe", "OuterWilds.exe" };
+		foreach (var executableName in executableNames)
+		{
+			var executablePath = Path.Combine(gamePath, executableName);
+			if (File.Exists(executablePath))
+			{
+				return Path.GetFileNameWithoutExtension(executablePath);
+			}
+		}
+
+		throw new FileNotFoundException($"Outer Wilds exe file not found in {gamePath}");
+	}
+
+	private static string GetDataPath(string gamePath)
+		=> Path.Combine(gamePath, $"{GetDataDirectoryName()}");
+
+	private static string GetDataDirectoryName()
+	{
+		var gamePath = AppDomain.CurrentDomain.BaseDirectory;
+		return $"{GetExecutableName(gamePath)}_Data";
+	}
+}
