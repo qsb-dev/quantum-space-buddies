c3c8e6a3758a164c688d56ba7709622ccc34df07
_nebula <41904486+misternebula@users.noreply.github.com>
2023-03-08 21:02:00 +0000

    include count in hash message

--- QSB/WorldSync/WorldObjectsHashMessage.cs
+++ QSB/WorldSync/WorldObjectsHashMessage.cs
@@ -8,21 +8,21 @@ namespace QSB.WorldSync;
 /// <summary>
 /// sends QSBWorldSync.WorldObjectsHash to the server for sanity checking
 /// </summary>
-internal class WorldObjectsHashMessage : QSBMessage<(string managerName, string hash)>
+internal class WorldObjectsHashMessage : QSBMessage<(string managerName, string hash, int count)>
 {
-	public WorldObjectsHashMessage(string managerName, string hash) : base((managerName, hash)) => To = 0;
+	public WorldObjectsHashMessage(string managerName, string hash, int count) : base((managerName, hash, count)) => To = 0;
 
 	public override void OnReceiveRemote()
 	{
 		Delay.RunWhen(() => QSBWorldSync.AllObjectsAdded, () =>
 		{
-			var serverHash = QSBWorldSync.ManagerHashes[Data.managerName];
+			var (hash, count) = QSBWorldSync.ManagerHashes[Data.managerName];
 
-			if (serverHash != Data.hash)
+			if (hash != Data.hash)
 			{
 				// oh fuck oh no oh god
-				DebugLog.ToConsole($"Kicking {From} because their WorldObjects hash for {Data.managerName} is wrong. (server:{serverHash}, client:{Data.hash})", MessageType.Error);
-				new PlayerKickMessage(From, $"WorldObject hash error for {Data.managerName}. (Server:{serverHash}, Client:{Data.hash})").Send();
+				DebugLog.ToConsole($"Kicking {From} because their WorldObjects hash for {Data.managerName} is wrong. (Server:{hash} count:{count}, Client:{Data.hash} count:{Data.count})", MessageType.Error);
+				new PlayerKickMessage(From, $"WorldObject hash error for {Data.managerName}. (Server:{hash} count:{count}, Client:{Data.hash}, count:{Data.count})").Send();
 			}
 		});
 	}
