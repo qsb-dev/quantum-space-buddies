cdc774e91c2bd216a5b2239451ee60c6d8d863c4
_nebula <41904486+misternebula@users.noreply.github.com>
2023-06-11 01:21:16 +0100

    Update QSBNetworkManager.cs

--- QSB/QSBNetworkManager.cs
+++ QSB/QSBNetworkManager.cs
@@ -80,7 +80,6 @@ public class QSBNetworkManager : NetworkManager, IAddComponentOnStart
 
 		{
 			_steamTransport = gameObject.AddComponent<FizzySteamworks>();
-			_steamTransport.UseNextGenSteamNetworking = false;
 		}
 
 		transport = QSBCore.UseKcpTransport ? _kcpTransport : _steamTransport;
