7dfbba52c98685d3571c59b4dcef92727d5d6c30
_nebula <41904486+misternebula@users.noreply.github.com>
2023-07-04 20:05:49 +0100

    make OnModStart addon patches work

--- QSB/QSBCore.cs
+++ QSB/QSBCore.cs
@@ -167,6 +167,7 @@ public class QSBCore : ModBehaviour
 
 		// init again to get addon patches
 		QSBPatchManager.Init();
+		QSBPatchManager.DoPatchType(QSBPatchTypes.OnModStart);
 
 		MenuApi = ModHelper.Interaction.TryGetModApi<IMenuAPI>(ModHelper.Manifest.Dependencies[0]);
 
