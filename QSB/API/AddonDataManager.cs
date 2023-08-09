8cef0f9a6d05cec2c8ff19324dc62ba9b4505674
JohnCorby <williamforgames6@gmail.com>
2023-08-09 13:29:46 -0700

    remove debug message for addon data, addons dev should just do this themselves

--- QSB/API/AddonDataManager.cs
+++ QSB/API/AddonDataManager.cs
@@ -11,7 +11,6 @@ public static class AddonDataManager
 
 	public static void OnReceiveDataMessage(int hash, object data, uint from)
 	{
-		DebugLog.DebugWrite($"Received addon message of with hash {hash} from {from}!");
 		if (!_handlers.TryGetValue(hash, out var handler))
 		{
 			DebugLog.DebugWrite($"unknown addon message type with hash {hash}", MessageType.Error);
@@ -22,7 +21,6 @@ public static class AddonDataManager
 
 	public static void RegisterHandler<T>(int hash, Action<uint, T> handler)
 	{
-		DebugLog.DebugWrite($"Registering addon message handler for hash {hash} with type {typeof(T).Name}");
 		_handlers.Add(hash, (from, data) => handler(from, (T)data));
 	}
 }
