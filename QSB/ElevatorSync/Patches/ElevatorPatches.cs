3ce50935c684b87b4b1e3d3bc9a977f4a0658a6e
Moonstone Studios <61355352+MoonstoneStudios@users.noreply.github.com>
2023-10-28 18:36:47 -0700

    Elevator initializer no longer sticks in place
    Revert of previous commit (c61d228fd8690ae72533b1792a112523b81b4d93)
    
    The person who initialized the elevator will drift in front of the elevator again. This is base game behavior.

--- QSB/ElevatorSync/Patches/ElevatorPatches.cs
+++ QSB/ElevatorSync/Patches/ElevatorPatches.cs
@@ -3,7 +3,6 @@ using QSB.ElevatorSync.Messages;
 using QSB.ElevatorSync.WorldObjects;
 using QSB.Messaging;
 using QSB.Patches;
-using QSB.Utility;
 using QSB.WorldSync;
 
 namespace QSB.ElevatorSync.Patches;
@@ -25,23 +24,5 @@ public class ElevatorPatches : QSBPatch
 		var isGoingUp = __instance._goingToTheEnd;
 		var qsbElevator = __instance.GetWorldObject<QSBElevator>();
 		qsbElevator.SendMessage(new ElevatorMessage(isGoingUp));
-	}
-
-	[HarmonyPrefix]
-	[HarmonyPatch(typeof(Elevator), nameof(Elevator.AttachPlayerAndStartLift))]
-	public static void Elevator_AttachPlayerAndStartLift(Elevator __instance)
-	{
-		// attach player to their current position instead of gliding them
-		// to the attach point.
-		var attachPoint = __instance._attachPoint;
-		var qsbElevator = __instance.GetWorldObject<QSBElevator>();
-		attachPoint.transform.position = Locator.GetPlayerTransform().position;
-
-		// Runs when the lift/elevator is done moving.
-		// Reset the position of the attach point.
-		Delay.RunWhen(() => !__instance.enabled, () =>
-		{
-			attachPoint.transform.localPosition = qsbElevator.originalAttachPosition;
-		});
 	}
 }
