2f0ac830310a8abb80753fa9457ac67108b51ce8
Will Corby <williamforgames6@gmail.com>
2025-02-25 20:23:49 -0800

    Update Util.cs

--- SteamTransport/Util.cs
+++ SteamTransport/Util.cs
@@ -67,7 +67,7 @@ public static class Util
 			}
 		});
 
-		// 50% change of doing all, delay .5 seconds
+		// 20% change of doing all, all delays are 200 ms. leads to about 1 second of rtt ping if enables on both ends.
 		if (transport.DoFakeNetworkErrors)
 		{
 			var floatHandle = GCHandle.Alloc((float)20, GCHandleType.Pinned);
